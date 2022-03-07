ba7e893972cbd1c541260c823483f2b80991c81f
EIzharikov <egor.zharikov@inbox.ru>
2022-03-07 15:16:47 +0300

    added prepare_enviroment,validate_config, init for Crawler, Crawler.find_articles

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,9 @@
 """
 Scrapper implementation
 """
-
+import json
+import os
+import requests
 
 class IncorrectURLError(Exception):
     """
@@ -26,7 +28,9 @@ class Crawler:
     Crawler implementation
     """
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.seed_urls = seed_urls
+        self.max_articles = max_articles
+        self.urls = []
 
     def _extract_url(self, article_bs):
         pass
@@ -35,7 +39,20 @@ class Crawler:
         """
         Finds articles
         """
-        pass
+        for url in self.seed_urls:
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome'
+                              '/97.0.4692.99 Safari/537.36 OPR/83.0.4254.70',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;'
+                          'q=0.8,applicat '
+                          'ion/signed-exchange;v=b3;q=0.9',
+                'Acccept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
+            }
+            response = requests.get(url=url, headers=headers)
+            with open('ASSETS_PATH/url.html', 'w', encoding='utf-8') as file:
+                file.write(response.text)
+            self.urls.append(url)
 
     def get_search_urls(self):
         """
@@ -48,14 +65,23 @@ def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.rmdir('ASSETS_PATH')
+    except FileNotFoundError:
+        pass
+    os.mkdir('ASSETS_PATH')
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    try:
+        with open(crawler_path) as file:
+            config = json.load(file)
+        return config["seed_urls"], config["total_articles_to_find_and_parse"]
+    except IncorrectURLError or NumberOfArticlesOutOfRangeError or IncorrectNumberOfArticlesError:
+        return
 
 
 if __name__ == '__main__':
