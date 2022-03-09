d7bb0d525c00f73ea16636b8f64e9432ce72058d
esgolovleva <golovleva.katerina@mail.ru>
2022-03-09 19:15:49 +0300

    ctlr

--- ctlr_test.py
+++ ctlr_test.py
@@ -4,3 +4,6 @@ html_file = requests.get('https://www.m24.ru/news/').text
 with open('test.html', 'w', encoding='utf-8') as f:
     f.write(html_file)
 
+# idk where i should write my code
+# that is why i created this file
+# btw my website doesn't work
