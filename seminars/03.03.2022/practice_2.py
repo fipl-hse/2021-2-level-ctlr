7eda9a677a33352dd45d80399c5416cb684c9acd
AnnaSizykh <aasizykh@edu.hse.ru>
2022-03-10 13:05:36 +0300

    extracting web page

--- seminars/03.03.2022/practice_2.py
+++ seminars/03.03.2022/practice_2.py
@@ -1,23 +1,34 @@
 # TASK 1
 # Make the code pass even if fails
+try:
+    a = None
 
-a = None
+    #print(a.split())
 
-print(a.split())
+    #print(a / 0)
 
-print(a / 0)
+    #print(int('abc'))
 
-print(int('abc'))
+    d = {}
 
-d = {}
+    #print(d['name'])
 
-print(d['name'])
+    l = []
 
-l = []
+    #print(l[100])
 
-print(l[100])
+    print(l[None])
+except AttributeError:
+    print('This is not a text')
+except TypeError as e:
+    print('Impossible action for the variable\'s type')
+    print(e)
+except ValueError:
+    print('Impossible action for this value')
+except (KeyError, IndexError):
+    print('The dict or list does not have such item')
 
-print(l[None])
+
 
 
 # TASK 2
@@ -26,13 +37,22 @@ def count_evens(numbers: list) -> int:
     Return the number of even (четный) numbers in the given list.
     If the argument is not a list, throw ValueError
     """
-    pass
+    if not isinstance(numbers, list):
+        raise ValueError('List type variable is required')
+    evens = 0
+    for number in numbers:
+        if number % 2 == 0:
+            evens += 1
+    print(evens)
+    return evens
 
 
 assert count_evens([2, 1, 2, 3, 4]), 3
 assert count_evens([2, 2, 0]), 3
-assert count_evens([1, 3, 5]), 0
-
+try:
+    count_evens(3)
+except ValueError:
+    print('We cannot detect a list')
 
 # TASK 1
 '''
@@ -40,7 +60,24 @@ Write a function `find_sum` that returns the sum of three numbers.
 However, 13 is unlucky, so if one of the values is
 13, then the function throws `ThirteenError`.
 '''
+class ThirteenError(Exception):
+    ''''Unlucky number'''
+    pass
+
+def find_sum(n1, n2, n3):
+    summa = 0
+    numbers = [n1, n2, n3]
+    try:
+        for i in range(3):
+            if numbers[i] == 13:
+                raise ThirteenError
+            summa += numbers[i]
+    except:
+        print('Error')
+        return None
+
+    return summa
 
 # Uncomment these lines when function is ready to check
-# print(find_sum(1, 2, 3))  # 6
-# print(find_sum(1, 2, 13))  # raises ThirteenError exception
+print(find_sum(1, 2, 3))  # 6
+print(find_sum(1, 2, 13))  # raises ThirteenError exception
