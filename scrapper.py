bbef614ee860adb728165f62d21ba123708d4e50
KarinaZakirova <90447262+KarinaZakirova@users.noreply.github.com>
2022-03-11 01:12:35 +0300

    for homework

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,11 @@
 """
 Scrapper implementation
 """
+import json
+import requests
+import re
+import os
+from constants import CRAWLER_CONFIG_PATH, ASSETS_PATH
 
 
 class IncorrectURLError(Exception):
@@ -26,7 +31,9 @@ class Crawler:
     Crawler implementation
     """
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.seed_urls = seed_urls
+        self.max_articles = max_articles
+        self.urls = []
 
     def _extract_url(self, article_bs):
         pass
@@ -35,7 +42,25 @@ class Crawler:
         """
         Finds articles
         """
-        pass
+        html = "https://languagejournal.spbu.ru/issue/view/682"
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " \
+                     "(KHTML, like Gecko) Chrome/96.0.4664.174 YaBrowser/22.1.4.837 " \
+                     "Yowser/2.5 Safari/537.36"
+        accept = "width=device-width, initial-scale=1.0"
+        accept_encoding = "utf-8"
+        accept_language = "ru-RU"
+        headers = \
+            {
+            'user-agent': user_agent,
+            'accept': accept,
+            'accept-encoding': accept_encoding,
+            'accept-language': accept_language
+            }
+        for seed_url in self.seed_urls:
+            response = requests.get(html, headers=headers)
+            with open(ASSETS_PATH, 'w', encoding='utf-8') as f:
+                f.write(response.text)
+            self.urls.append(seed_url)
 
     def get_search_urls(self):
         """
@@ -48,14 +73,43 @@ def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.rmdir(base_path)
+    except FileNotFoundError:
+        os.mkdir(base_path)
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    try:
+        with open(crawler_path) as file:
+            config = json.load(file)
+
+        seed_urls = config["seed_urls"]
+        total_articles = config["total_articles_to_find_and_parse"]
+
+        for seed_url in seed_urls:
+            if not re.match(r'https?://', seed_url):
+                raise IncorrectURLError
+
+        if not isinstance(total_articles, int):
+            raise IncorrectNumberOfArticlesError
+
+        if total_articles > 300:
+            raise NumberOfArticlesOutOfRangeError
+
+        prepare_environment(ASSETS_PATH)
+
+        return seed_urls, total_articles
+
+    except IncorrectURLError:
+        print("IncorrectURLError")
+    except NumberOfArticlesOutOfRangeError:
+        print("NumberOfArticlesOutOfRangeError")
+    except IncorrectNumberOfArticlesError:
+        print("IncorrectNumberOfArticlesError")
 
 
 if __name__ == '__main__':
