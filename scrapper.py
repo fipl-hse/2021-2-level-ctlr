823b1e5245f4fb0cc44e16854806b15852dad820
KseniaEremeeva <irtn2002@yandex.ru>
2022-03-13 22:36:59 +0300

    Added realization of finding articles and urls

--- scrapper.py
+++ scrapper.py
@@ -2,12 +2,14 @@
 Scrapper implementation
 """
 
+import requests, urllib.error
+from bs4 import BeautifulSoup
+
 
 class IncorrectURLError(Exception):
     """
     Seed URL does not match standard pattern
     """
-
 
 
 class NumberOfArticlesOutOfRangeError(Exception):
@@ -27,7 +29,8 @@ class Crawler:
     Crawler implementation
     """
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.seed_urls = seed_urls
+        self.max_articles = max_articles
 
     def _extract_url(self, article_bs):
         pass
@@ -36,13 +39,44 @@ class Crawler:
         """
         Finds articles
         """
-        pass
 
-    def get_search_urls(self):
+        url = 'https://aif.ru/dosug'
+        response = requests.get(url, headers={
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+            "accept-encoding": "gzip, deflate, br", "accept-language": 'en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7,fr;q=0.6',
+            "cookie": "__ddg1=3z7ohwF248ukR81grhvj; CookieMessenger=; _ga=GA1.2.1866016929.1646422780; _ym_d=1646422780; _ym_uid=1620758162826342715; __ddgid=MhwOCqWUpVMS0HAn; subscription_popup_min_state=week_view1; subscription_popup_state=%7B%22key%22%3A%22week_view1%22%2C%22timeout%22%3A1647599958264%7D; __ddgmark=JrhVBqNQJzM3xeRX; __ddg5=T7GglOZhSXdoo9HS; aif_sid=b806513bc5db4b27668b98a5d3e86f74; _gid=GA1.2.1353469175.1647194832; _ym_isad=1; _ym_visorc=b; _gat_gtag_UA_3672159_1=1"},
+                                allow_redirects=True)
+        with open('index.html', 'w', encoding='utf-8') as file:
+            file.write(response.text)
+
+        return response, url
+
+
+    def get_search_urls(self, response, url):
         """
         Returns seed_urls param
         """
-        pass
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        div_bs = soup.find('div', class_ ="content")
+        print(div_bs.text)
+
+        all_links = soup.find_all('a')
+        print(len(all_links))
+
+        links = []
+
+        for link in all_links:
+            try:
+                # print(url + link['href'])
+                links.append(link['href'])
+            except (KeyError, urllib.error.URLError, urllib.error.HTTPError):
+                print('Found incorrect link')
+
+            for elements in links:
+                if ('http://' and 'https://') not in elements:
+                    links.remove(elements)
 
 
 def prepare_environment(base_path):
