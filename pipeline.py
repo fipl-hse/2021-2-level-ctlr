c6a81e025e0e0560d3fa1f1f365f6be99fbf5bd3
DashaKrinochkina <dasha060402@mail.ru>
2022-03-14 20:27:31 +0300

    config

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,6 @@
 """
 Pipeline for text processing implementation
 """
-import requests
 
 
 class EmptyDirectoryError(Exception):
@@ -93,13 +92,7 @@ def validate_dataset(path_to_validate):
 
 
 def main():
-    html = 'https://lingngu.elpub.ru/jour/issue/archive'
-    response = requests.get(html, headers={'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 '
-                                                         '(KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'})
-    page_code = response.text
-
-    with open('page_code.html', 'w', encoding='utf-8') as file:
-        file.write(page_code)
+    pass
 
 
 if __name__ == "__main__":
--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,11 @@
 """
 Scrapper implementation
 """
+import json
+import re
+import os
+import requests
+from constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
 
 
 class IncorrectURLError(Exception):
@@ -26,7 +31,9 @@ class Crawler:
     Crawler implementation
     """
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.seed_urls = seed_urls
+        self.max_articles = max_articles
+        self.urls = []
 
     def _extract_url(self, article_bs):
         pass
@@ -48,14 +55,45 @@ def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.rmdir(base_path)  # removes a folder
+    except FileNotFoundError:
+        os.mkdir(base_path)  # creates new if it doesn't exist
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    with open(crawler_path, 'r', encoding='utf-8') as dictionary:
+        config = json.load(dictionary)  # load is for decoding
+
+    seed_urls = config['seed_urls']
+    max_articles = config['total_articles_to_find_and_parse']
+
+    for url in seed_urls:
+        if not re.match(r'https?://', url):  # https or http
+            raise IncorrectURLError('IncorrectURLError')
+
+    if max_articles > 200:
+        raise NumberOfArticlesOutOfRangeError('NumberOfArticlesOutOfRangeError')
+
+    if not isinstance(max_articles, int):
+        raise IncorrectNumberOfArticlesError('IncorrectNumberOfArticlesError')
+
+    return seed_urls, max_articles
+
+
+'''
+    html = 'https://lingngu.elpub.ru/jour/issue/view/18/showToc'
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/99.0.4844.51 Safari/537.36'}
+
+    response = requests.get(html, headers)
+    page_code = response.text
+
+    with open('page_code.html', 'w', encoding='utf-8') as file:
+        file.write(page_code)'''
 
 
 if __name__ == '__main__':
