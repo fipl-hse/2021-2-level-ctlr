96ae0212a21a35e0b0036cd3c8549df66f7287ef
tanynova99 <balabolka2000@gmail.com>
2022-03-15 02:14:46 +0300

    Mandatory config and dir checks added.

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,8 @@
 Scrapper implementation
 """
 
+impport re
+import os
 
 class IncorrectURLError(Exception):
     """
@@ -49,15 +51,40 @@ def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    if os.path.isdir(base_path):
+        if not os.listdir(base_path):
+            pass
+        else:
+            os.rmdir(base_path)
+            os.mkdir(base_path)
+    else:
+        os.mkdir(base_path)
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
 
+    with open(crawler_path) as file:
+        config = json.load(file)
+
+    urls = config["seed_urls"]
+    articles = config["total_articles_to_find_and_parse"]
+    http_regex = r'http[s]?://+'
+
+    if not isinstance(articles, int):
+        raise IncorrectNumberOfArticlesError
+
+    if articles > 1000 or articles < 0:
+        raise NumberOfArticlesOutOfRangeError
+
+    for url in urls:
+        check = http_regex.search(url)
+        if not check:
+            raise IncorrectURLError
+
+    return seed_urls, total_articles
 
 if __name__ == '__main__':
     # YOUR CODE HERE
