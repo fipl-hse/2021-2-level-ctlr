62c0c53a64169f7309e86d97f5eefd974d3faa69
Anastasia <awake777.20@gmail.com>
2022-03-15 22:28:56 +0300

    Papapapa papapa pa papapam

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,14 @@
 Scrapper implementation
 """
 
+import json
+import requests
+import re
+import os
+import time
+from constants import CRAWLER_CONFIG_PATH, ASSETS_PATH
+from bs4 import BeautifulSoup
+
 
 class IncorrectURLError(Exception):
     """
@@ -26,7 +34,9 @@ class Crawler:
     Crawler implementation
     """
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.seed_urls = seed_urls
+        self.max_articles = max_articles
+        self.urls = []
 
     def _extract_url(self, article_bs):
         pass
@@ -35,7 +45,23 @@ class Crawler:
         """
         Finds articles
         """
-        pass
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) '
+            'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'ru,en;q=0.9,de;q=0.8'
+        }
+
+        for seed_url in self.seed_urls:
+            response = requests.get(url=seed_url, headers=headers)
+            time.sleep(3)
+            if not response.ok:
+                print("Something went wrong...")
+            soup = BeautifulSoup(response.text, "lxml")
+            main_block_bs = soup.find('div', {'class': 'cat-children'})
+            for link in main_block_bs.find_all("a"):
+                self.urls.append('https://l.jvolsu.com/' + link.get('href'))
+
 
     def get_search_urls(self):
         """
@@ -48,14 +74,33 @@ def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.rmdir(base_path)
+    except FileNotFoundError:
+        os.mkdir(base_path)
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    with open(crawler_path, "r", encoding="utf-8") as file:
+        config = json.load(file)
+    seed_urls = config["seed_urls"]
+    number_of_articles = config["total_articles_to_find_and_parse"]
+    if not seed_urls or not isinstance(seed_urls, list):
+        raise IncorrectURLError
+    for seed_url in seed_urls:
+        if not isinstance(seed_url, str):
+            raise IncorrectURLError
+        norm_url = re.match(r"https?://", seed_url)
+        if not norm_url:
+            raise IncorrectURLError
+    if not isinstance(number_of_articles, int) or number_of_articles <= 0:
+        raise IncorrectNumberOfArticlesError
+    if number_of_articles > 200:
+        raise NumberOfArticlesOutOfRangeError
+    return seed_urls, number_of_articles
 
 
 if __name__ == '__main__':
