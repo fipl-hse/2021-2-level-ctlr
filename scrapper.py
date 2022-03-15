808f7e53c11d8fc6cf0e182b05e86bf61ee477b7
Lyubov Talanina <72165770+lyutl@users.noreply.github.com>
2022-03-15 16:48:26 +0300

    stage 1 i guess

--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,10 @@ Scrapper implementation
 """
 
 import requests
+import json
+import os
+import re
+from constants import CRAWLER_CONFIG_PATH, ASSETS_PATH
 
 
 class IncorrectURLError(Exception):
@@ -28,7 +32,9 @@ class Crawler:
     Crawler implementation
     """
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.seed_urls = seed_urls
+        self.max_articles = max_articles
+        self.urls = []
 
     def _extract_url(self, article_bs):
         pass
@@ -37,13 +43,13 @@ class Crawler:
         """
         Finds articles
         """
-        html = 'https://vestnik.lunn.ru/arhiv-zhurnala/2021-god/vypusk-56-iv-kvartal-2021-g/'
-        response = requests.get(html, headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit'
-                                                             '/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'})
-        page_code = response.text
-
-        with open('page_code.html', 'w', encoding='utf-8') as file:
-            file.write(page_code)
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/'
+                          '537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/'
+                      'avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'ru,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7,la;q=0.6'}
 
     def get_search_urls(self):
         """
@@ -56,24 +62,40 @@ def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.rmdir(base_path)
+    except FileNotFoundError:
+        os.mkdir(base_path)
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    try:
+        with open(crawler_path, 'r', encoding='utf-8') as config:
+            conf = json.load(config)
 
+        seed_urls = conf["seed_urls"]
+        max_articles = conf["total_articles_to_find_and_parse"]
 
-def main():
-    html = 'https://vestnik.lunn.ru/arhiv-zhurnala/2021-god/vypusk-56-iv-kvartal-2021-g/'
-    response = requests.get(html, headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit'
-                                                         '/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'})
-    page_code = response.text
+        for url in seed_urls:
+            right_url = re.match(r'https?://', url)
+            if not right_url:
+                raise IncorrectURLError
 
-    with open('page_code.html', 'w', encoding='utf-8') as file:
-        file.write(page_code)
+        if not isinstance(max_articles, int):
+            raise IncorrectNumberOfArticlesError
+
+        if max_articles > 200:
+            raise IncorrectNumberOfArticlesError
+
+    except IncorrectURLError:
+        print('Incorrect URL')
+    except NumberOfArticlesOutOfRangeError:
+        print('Number of articles out of range')
+    except IncorrectNumberOfArticlesError:
+        print('Incorrect number of articles')
 
 
 if __name__ == '__main__':
