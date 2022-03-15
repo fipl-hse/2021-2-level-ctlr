8ccb6ad899e822adaa053fa47f7ffb01417032dd
Vasilisa-Blyudova <90454966+Vasilisa-Blyudova@users.noreply.github.com>
2022-03-15 22:13:45 +0300

    tests and ArticleParser

--- scrapper.py
+++ scrapper.py
@@ -4,6 +4,13 @@ Scrapper implementation
 
 import json
 import requests
+import os
+from bs4 import BeautifulSoup
+from constants import ASSETS_PATH
+
+
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                                 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
 
 class IncorrectURLError(Exception):
     """
@@ -28,24 +35,24 @@ class Crawler:
     Crawler implementation
     """
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.seed_urls = seed_urls
+        self.total_max_articles = max_articles
+        self.urls = []
 
     def _extract_url(self, article_bs):
-        pass
+        class_bs = article_bs.find('div', class_='container-fluid').find_all('li')
+        for link_search_bs in class_bs:
+            self.urls.append('http://www.vestnik.vsu.ru' + link_search_bs.find('a')['href'])
+
 
     def find_articles(self):
         """
         Finds articles
         """
-        # get data from website
-        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                                 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
-        response = requests.get('http://www.vestnik.vsu.ru/content/lingvo/index_ru.asp', headers=headers)
-        # print(response.status_code)
-
-        with open('index.html', 'w', encoding='utf-8') as file:
-            file.write(response.text)
-        pass
+        for seed_url in self.seed_urls:
+            response = requests.get(seed_url, headers=headers)
+            article_bs = BeautifulSoup(response.text, 'html.parser')
+            self._extract_url(article_bs)
 
     def get_search_urls(self):
         """
@@ -53,12 +60,33 @@ class Crawler:
         """
         pass
 
+class ArticleParser:
+    def __init__(self, article, full_url, i):
+        self.article_url = full_url
+        self.article_id = i
+
+    def _fill_article_with_text(self, article_bs):
+        pass
+
+    def _fill_article_with_meta_information(self, article_bs):
+        pass
+
+    def parser(self):
+        article_bs = BeautifulSoup(requests.get(self.article_url, headers=headers).content, 'lxml')
+        self._fill_article_with_text(article_bs)
+        self._fill_article_with_meta_information(article_bs)
+        return self.article_url
+    pass
+
 
 def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.rmdir(ASSETS_PATH)
+    except FileNotFoundError:
+        os.mkdir(ASSETS_PATH)
 
 def validate_config(crawler_path):
     """
@@ -68,22 +96,22 @@ def validate_config(crawler_path):
         scrapper_config = json.load(file)
 
     seed_urls = scrapper_config["seed_urls"]
-    total_articles = scrapper_config["total_articles_to_find_and_parse"]
+    max_articles = scrapper_config["total_articles_to_find_and_parse"]
 
-    if not seed_urls:
+    if not seed_urls or not isinstance(seed_urls, list):
         raise IncorrectURLError
 
     for seed_url in seed_urls:
         if not isinstance(seed_url, str):
             raise IncorrectURLError
 
-    if not isinstance(total_articles, int):
+    if not isinstance(max_articles, int) or (max_articles == 0):
         raise IncorrectNumberOfArticlesError
 
-    if not 0 < total_articles <= 100:
+    if not 0 < max_articles <= 100:
         raise NumberOfArticlesOutOfRangeError
 
-    return seed_urls, total_articles
+    return seed_urls, max_articles
 
 if __name__ == '__main__':
     # YOUR CODE HERE
