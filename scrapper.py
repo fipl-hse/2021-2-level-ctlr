c992f04a524a04447a57e8ca96de7b29270eae29
Vlados13iq <znaminvlad@gmail.com>
2022-03-15 18:15:06 +0300

    done something

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,8 @@
 """
 Scrapper implementation
 """
-
+import os, json, requests
+from constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
 
 class IncorrectURLError(Exception):
     """
@@ -19,8 +20,6 @@ class IncorrectNumberOfArticlesError(Exception):
     """
     Total number of articles to parse in not integer
     """
-#lets goooo
-#depression
 
 
 class Crawler:
@@ -28,7 +27,9 @@ class Crawler:
     Crawler implementation
     """
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.seed_urls = seed_urls
+        self.max_articles = max_articles
+        self.urls = []
 
     def _extract_url(self, article_bs):
         pass
@@ -43,21 +44,38 @@ class Crawler:
         """
         Returns seed_urls param
         """
-        pass
+        return self.seed_urls
 
 
 def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.rmdir(base_path)
+    except FileNotFoundError:
+        pass
+    os.mkdir(base_path)
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    with open(crawler_path, 'r', encoding='utf-8') as file:
+        config = json.load(file)
+    seed_urls = config['seed_urls']
+    max_articles = config['total_articles_to_parse']
+    if not isinstance(max_articles, int) or max_articles <= 0:
+        raise IncorrectNumberOfArticlesError
+    if not isinstance(seed_urls, list) or not seed_urls:
+        raise IncorrectURLError
+    if max_articles > 100:
+        raise NumberOfArticlesOutOfRangeError
+    for urls in seed_urls:
+        if urls[0:8] != 'https://' or urls[0:7] != 'http://':
+            raise IncorrectURLError
+    return seed_urls, max_articles
 
 
 if __name__ == '__main__':
