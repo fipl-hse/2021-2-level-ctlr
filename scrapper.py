1e05180a17eb1873b3e643f2e255da966ec3311f
VSvir <VSslava612@yandex.ru>
2022-03-15 19:40:31 +0300

    Fixed validate_config for Scrapper class Created empty functions for ArticleParser class Created HTMLParser class

--- scrapper.py
+++ scrapper.py
@@ -9,6 +9,8 @@ import re
 import requests
 
 from constants import CRAWLER_CONFIG_PATH, ASSETS_PATH
+from core_utils import article
+from bs4 import BeautifulSoup
 
 
 class IncorrectURLError(Exception):
@@ -49,9 +51,11 @@ class Crawler:
         Finds articles
         """
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Acccept-Encoding': 'gzip, deflate',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'
+                      'image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'ru,en-GB;q=0.9,en;q=0.8,en-US;q=0.7'
         }
 
@@ -72,7 +76,17 @@ class ArticleParser:
     def __init__(self, full_url, i):
         self.article_url = full_url
         self.article_id = i
-        self.article = ''
+        self.article = article.Article
+
+    def _fill_article_with_text(self, article_bs):
+        pass
+
+    """def parse(self):
+        web_page = requests.get(self.article_url, headers=)"""
+
+
+class HTMLParser:
+    pass
 
 
 def prepare_environment(base_path):
@@ -80,8 +94,9 @@ def prepare_environment(base_path):
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
     try:
+        os.mkdir(base_path)
+    except OSError:
         os.rmdir(base_path)
-    except FileNotFoundError:
         os.mkdir(base_path)
 
 
@@ -89,37 +104,32 @@ def validate_config(crawler_path):
     """
     Validates given config
     """
-    try:
-        with open(crawler_path, 'r', encoding='utf-8') as config:
-            data = json.load(config)
+    with open(crawler_path, 'r', encoding='utf-8') as config:
+        data = json.load(config)
 
-        seed_urls = data["seed_urls"]
-        max_articles = data["total_articles_to_find_and_parse"]
+    seed_urls = data["seed_urls"]
+    max_articles = data["total_articles_to_find_and_parse"]
 
-        for url in seed_urls:
-            url_validation = re.match(r'https?://', url)
-            if not url_validation:
-                raise IncorrectURLError
+    if not isinstance(seed_urls, list):
+        raise IncorrectURLError
+    if not seed_urls:
+        raise IncorrectURLError
+    for url in seed_urls:
+        url_validation = re.match(r'https?://', url)
+        if not url_validation:
+            raise IncorrectURLError
 
-        if not isinstance(max_articles, int):
-            raise IncorrectNumberOfArticlesError
+    if not isinstance(max_articles, int) or 0 >= max_articles:
+        raise IncorrectNumberOfArticlesError
 
-        if max_articles > 300:
-            raise NumberOfArticlesOutOfRangeError
+    if max_articles > 300:
+        raise NumberOfArticlesOutOfRangeError
 
-        prepare_environment(ASSETS_PATH)
-
-        return seed_urls, max_articles
-
-    except IncorrectURLError:
-        print('Incorrect URL')
-    except IncorrectNumberOfArticlesError:
-        print('Incorrect number of articles error')
-    except NumberOfArticlesOutOfRangeError:
-        print('Number of articles out of range')
+    return seed_urls, max_articles
 
 
-crawler = Crawler(validate_config(CRAWLER_CONFIG_PATH)[0], validate_config(CRAWLER_CONFIG_PATH)[1])
+#  crawler = Crawler(validate_config(CRAWLER_CONFIG_PATH)[0],
+#  validate_config(CRAWLER_CONFIG_PATH)[1])
 
 
 if __name__ == '__main__':
