12f7d4db838abb7067fbe5cfa39ebb5da42e7597
KoykovaAleksandra <madam.koykova@mail.ru>
2022-03-16 16:38:46 +0300

    Add some changes

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,10 @@
 """
 Scrapper implementation
 """
+import os
+import re
+import requests
+import json
 
 
 class IncorrectURLError(Exception):
@@ -25,8 +29,11 @@ class Crawler:
     """
     Crawler implementation
     """
+
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.seed_urls = seed_urls
+        self.max_articles = max_articles
+        self.urls = []
 
     def _extract_url(self, article_bs):
         pass
@@ -48,14 +55,35 @@ def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.rmdir(base_path)
+    except FileNotFoundError:
+        pass
+    finally:
+        os.mkdir(base_path)
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    with open(crawler_path) as file:
+        configuration = json.load(file)
+
+    seed_urls = configuration['seed_urls']
+    max_articles = configuration['total_articles_to_find_and_parse']
+
+    if not isinstance(max_articles, int):
+        raise IncorrectNumberOfArticlesError
+
+    if max_articles > 200:
+        raise NumberOfArticlesOutOfRangeError
+
+    part_of_string = re.compile(r'^https?://')
+    for seed_url in seed_urls:
+        if not part_of_string.search(seed_url):
+            raise IncorrectURLError
+
 
 
 if __name__ == '__main__':
