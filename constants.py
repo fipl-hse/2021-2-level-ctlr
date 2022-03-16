759a4fb69f490f7899f2d5a109b34819ffb21389
IrinaKurnakova <ikurnakova2002@mail.ru>
2022-03-16 19:40:58 +0300

    constants.py

--- constants.py
+++ constants.py
@@ -7,3 +7,9 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).parent
 ASSETS_PATH = PROJECT_ROOT / 'tmp' / 'articles'
 CRAWLER_CONFIG_PATH = PROJECT_ROOT / 'scrapper_config.json'
+HEADERS = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) '
+            'Gecko/20100101 Firefox/33.0',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,fr;q=0.6'}
