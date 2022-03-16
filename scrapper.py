ccba6cd8fa7a376bc25852e428f13b6088b0d988
awakenedlilac <90451983+awakenedlilac@users.noreply.github.com>
2022-03-16 09:14:22 +0300

    added some lines

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,13 @@
 """
 Scrapper implementation
 """
-
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+import os
+from constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
+from core_utils import article
 
 class IncorrectURLError(Exception):
     """
@@ -26,10 +32,17 @@ class Crawler:
     Crawler implementation
     """
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.seed_urls = seed_urls
+        self.max_articles = max_articles
+        self.urls = []
 
     def _extract_url(self, article_bs):
-        pass
+        content = article_bs.find_all('div', id = '_id_article_listing')
+        for article in content:
+            all_links = article.find_all('a')
+            for link in all_links:
+                self.urls.append('https://gazeta.ru' + link ['href'])
+
 
     def find_articles(self):
         """
@@ -43,21 +56,64 @@ class Crawler:
         """
         pass
 
+class ArticleParser:
+    def __init__(self, full_url, i):
+        self.article_url = full_url
+        self.article_id = i
+        self.article = article.Article
+
+    def _fill_article_with_text(self, article_bs):
+        # self.article.save_raw()
+        pass
+
+    def parse(self):
+        response = requests.get(self.article_url)
+        response.encoding = 'utf-8'
+        with open(f'{ASSETS_PATH}/{self.article_id}_article_url.html', 'w', encoding='utf-16') as file:
+            file.write(response.text)
+        with open(f'{ASSETS_PATH}/{self.article_id}_article_url.html', encoding='utf-16') as file:
+            response = file.read()
+
+        article_bs = BeautifulSoup(response, 'html.parser')
+
+
+
+
 
 def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.rmdir(ASSETS_PATH)
+    except FileNotFoundError:
+        os.mkdir(ASSETS_PATH)
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    with open(crawler_path) as f:
+        config = json.load(f)
+    seed_urls = config['seed_urls']
+    max_articles = config['total_articles_to_find_and_parse']
+    for url in seed_urls:
+        correct_url = re.match(r'https://', url)
+        if not correct_url:
+            raise IncorrectURLError
+    if not isinstance(max_articles, int):
+        raise IncorrectNumberOfArticlesError
+    prepare_environment(ASSETS_PATH)
+    return seed_urls, max_articles
 
 
 if __name__ == '__main__':
     # YOUR CODE HERE
-    pass
+
+    # seed_urls = config['seed_urls']
+    # max_articles = config['total_articles']
+    # crawler = Crawler(seed_urls=seed_urls, total_max_articles=max_articles)
+    # crawler.find_articles()
+    # parser = ArticleParser(article_url=full_url, article_id=i)
+
