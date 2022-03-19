54dbb177877657c164d46be89024aaf75f15c903
lenazagg <lenazagovorova@gmail.com>
2022-03-19 11:47:12 +0300

    changes in scrapper

--- scrapper.py
+++ scrapper.py
@@ -4,17 +4,8 @@ Scrapper implementation
 import requests
 from bs4 import BeautifulSoup
 import re
-
-
-def main():
-    response = requests.get('https://vz.ru/')
-    print(response.status_code)
-
-    with open('index.html', 'w', encoding='utf-8') as f:
-        f.write(response.text)
-
-
-main()
+import json
+import os
 
 
 class IncorrectURLError(Exception):
@@ -72,14 +63,41 @@ def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.rmdir(base_path)  # removing directory
+    except FileNotFoundError:
+        pass
+    os.mkdir(base_path)  # creating directory
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    with open(crawler_path) as file:
+        config = json.load(file)
+
+    seed_urls = config['seed_urls']
+    max_articles = config['total_articles_to_find_and_parse']
+
+    if not isinstance(seed_urls, list):
+        raise IncorrectURLError
+
+    if not isinstance(max_articles, int):
+        raise IncorrectNumberOfArticlesError
+
+    for url in seed_urls:
+        correct_url = re.match(r'https://', url)
+        if not correct_url:
+            raise IncorrectURLError
+
+    if max_articles > 100:
+        raise NumberOfArticlesOutOfRangeError
+
+    if max_articles <= 0:
+        raise IncorrectNumberOfArticlesError
+
+    return seed_urls, max_articles
 
 
 if __name__ == '__main__':
