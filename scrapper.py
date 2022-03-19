8a69d82872fa87597083aa9272319fb1e5f6769e
DaryaKhodyreva <dashanasha2002@gmail.com>
2022-03-19 18:42:31 +0300

    changes part 1

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,9 @@
 Scrapper implementation
 """
 
+import requests
+from bs4 import BeautifulSoup
+
 
 class IncorrectURLError(Exception):
     """
@@ -26,16 +29,35 @@ class Crawler:
     Crawler implementation
     """
     def __init__(self, seed_urls, max_articles: int):
-        pass
+        self.max_articles = max_articles
+        self.seed_urls = seed_urls
+        self.urls = []
 
     def _extract_url(self, article_bs):
-        pass
+        urls_bs = article_bs.find_all('a', class_="item__link")
+        begin_link = "https://www.rbc.ru/"
+        urls_bs_all = []
+
+        for url_bs in urls_bs:
+            end_link = url_bs['href']
+            urls_bs_all.append(f'{begin_link}{end_link}')
+
+        return urls_bs_all
 
     def find_articles(self):
         """
         Finds articles
         """
-        pass
+        for seed_url in self.seed_urls:
+            response = requests.get(url=seed_url)
+
+            if not response.ok:
+                continue
+
+            soup_lib = BeautifulSoup(response.text, 'lxml')
+            article_urls = self._extract_url(soup_lib)
+            for article_url in article_urls:
+                self.urls.append(article_url)
 
     def get_search_urls(self):
         """
@@ -59,5 +81,4 @@ def validate_config(crawler_path):
 
 
 if __name__ == '__main__':
-    # YOUR CODE HERE
     pass
