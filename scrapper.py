c61028b6ca654fe2dd9e974893e1b91314781a74
DaryaKhodyreva <dashanasha2002@gmail.com>
2022-03-19 19:33:38 +0300

    changes part 2

--- scrapper.py
+++ scrapper.py
@@ -1,9 +1,12 @@
 """
 Scrapper implementation
 """
-
+import os
+import json
+import re
 import requests
 from bs4 import BeautifulSoup
+from constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
 
 
 class IncorrectURLError(Exception):
@@ -63,22 +66,48 @@ class Crawler:
         """
         Returns seed_urls param
         """
-        pass
+        return self.seed_urls
 
 
 def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    try:
+        os.removedirs(base_path)
+    except FileNotFoundError:
+        pass
+    finally:
+        os.makedirs(base_path)
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    with open(crawler_path) as file:
+        config = json.load(file)
+
+    seed_urls = config['seed_urls']
+    total_articles = config['total_articles_to_find_and_parse']
+
+    if not seed_urls:
+        raise IncorrectURLError
+    for article_url in seed_urls:
+        correct_url = re.match(r'https://', article_url)  # looking for pattern in the string
+        if not correct_url:
+            raise IncorrectURLError
+
+    if total_articles > 100:
+        raise NumberOfArticlesOutOfRangeError
+    if not isinstance(total_articles, int) or total_articles <= 0:
+        raise IncorrectNumberOfArticlesError
+
+    return seed_urls, total_articles
 
 
 if __name__ == '__main__':
-    pass
+    seed_urls, total_articles = validate_config(CRAWLER_CONFIG_PATH)
+    prepare_environment(ASSETS_PATH)
+    crawler = Crawler(seed_urls=seed_urls, max_articles=total_articles)
+    crawler.find_articles()
