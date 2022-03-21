a5439003797f6b79f35790341520a8b7b61cad5a
tanynova99 <balabolka2000@gmail.com>
2022-03-22 01:46:53 +0300

    Raw not working "bones" for parser

--- scrapper.py
+++ scrapper.py
@@ -8,11 +8,14 @@ import random
 import re
 import shutil
 import time
+import urllib.parse
 
 import requests
 from bs4 import BeautifulSoup
 
-from constants import HEADERS
+from constants import ASSETS_PATH, CRAWLER_CONFIG_PATH, DOMAIN, HEADERS
+from core_utils.article import Article
+from core_utils.pdf_utils import PDFRawFile
 
 
 class IncorrectURLError(Exception):
@@ -33,7 +36,7 @@ class IncorrectNumberOfArticlesError(Exception):
     """
 
 
-class Crawler:
+class PDFCrawler:
     """
     Crawler implementation
     """
@@ -42,18 +45,19 @@ class Crawler:
         self.seed_urls = seed_urls
         self.max_articles = max_articles
         self.urls = []
-        self.domain = ""
 
     def _extract_url(self, article_bs):
-        for article_link in article_bs.find_all("a", class_="article__download button-icon"):
-            self.urls.append(''.join([self.domain, article_link.get("href")]))
+
+        urls = []
+        for article_link in article_bs.find_all("a", class_="article_title"):
+            urls.append(''.join([DOMAIN, article_link.get("href")]))
+
+        return urls
 
     def find_articles(self):
         """
         Finds articles
         """
-        domain = urlparse(self.seed_urls[0])
-        self.domain = domain.scheme + "://" + domain.netloc + "/"
 
         for url in self.seed_urls:
             response = requests.get(url, headers=HEADERS)
@@ -93,7 +97,7 @@ def validate_config(crawler_path):
 
     urls = config["seed_urls"]
     articles = config["total_articles_to_find_and_parse"]
-    http_regex = r'http[s]?://+\.ru/.+'
+    http_regex = r"http[s]?://journals\.kantiana\.ru/."
 
     if not urls:
         raise IncorrectURLError
@@ -112,7 +116,7 @@ def validate_config(crawler_path):
     return urls, articles
 
 
-class HTMLParser:
+class HTMLWithPDFParser:
 
     def __init__(self, article_url, article_id):
         """
@@ -122,8 +126,63 @@ class HTMLParser:
         self.article_id = article_id
         self.article = Article(url=article_url, article_id=article_id)
 
+    def parse(self):
+        """
+        filling the class Article instance
+        """
+        response = requests.get(self.article_url)
+        article_bs = BeautifulSoup(response.text, 'html.parser')
+
+        self._fill_article_with_text(article_bs)
+        self._fill_article_with_meta_information(article_bs)
+
+        return self.article
+
+    def _fill_article_with_text(self, article_bs):
+        """
+        Scrap the text from PDF link embedded in article url
+        """
+        possible_pdfs = article_bs.find_all('a', class_="article-panel__item button-icon")
+
+        for pdf in possible_pdfs:
+            if "upload" in pdf["href"]:
+                pdf_raw = PDFRawFile(prf['href'], self.article_id)
+
+                pdf_raw.download()
+                pdf_text = pdf_raw.get_text()
+
+                text_only = pdf_text.split('Список литературы')
+                self.article.text = ''.join(text_only[:-1])
+                break
+
+    def _fill_article_with_meta_information(self, article_bs):
+        """
+        Add meta information to Article class instance
+        """
+        title = article_bs.find("h3", class_="article__title")
+        self.article.title = title.text
+
+        authors = article_bs.find_all("a", class_="link link_const article__author")
+        self.article.author = authors.text
+
+        date_raw = re.search("\d{4} Выпуск №\d", self.article.text)
+
+        # Only year is available, the № of issues per year doesn't correspond with months
+        if date_raw:
+            self.article.date = datetime.datetime.strptime(date_raw.group(0)[:4], '%Y')
+
 
 if __name__ == '__main__':
-    outer_seed_urls, outer_max_articles = validate_config("scrapper_config.json")
-    crawler = Crawler(outer_seed_urls, outer_max_articles)
+    # checking the environment
+    seed_urls, max_articles = validate_config(CRAWLER_CONFIG_PATH)
+
+    # initiating Crawler with PDF class instance and extract article links
+    crawler = PDFCrawler(seed_urls, max_articles)
     crawler.find_articles()
+
+    # extracting pdf, parsing pdf and saving text from every article link
+    # stored in PDFCrawler instance
+    for i, url in enumerate(crawler.urls):
+        parser = HTMLWithPDFParser(url, i + 1)
+        article = parser.parse()
+        article.save_raw()
