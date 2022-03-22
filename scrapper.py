91d0bb650610d21d3173f73c94df714dab080a5f
Vasilisa-Blyudova <90454966+Vasilisa-Blyudova@users.noreply.github.com>
2022-03-22 19:58:31 +0300

    changes

--- scrapper.py
+++ scrapper.py
@@ -12,7 +12,8 @@ from constants import CRAWLER_CONFIG_PATH, ASSETS_PATH
 from core_utils.article import Article
 from core_utils.pdf_utils import PDFRawFile
 
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
+
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
                          'like Gecko) Chrome/99.0.4844.74 Safari/537.36'}
 
 class IncorrectURLError(Exception):
@@ -56,7 +57,7 @@ class Crawler:
         Finds articles
         """
         for seed_url in self.seed_urls:
-            response = requests.get(seed_url, headers=HEADERS)
+            response = requests.get(seed_url, headers=headers)
             article_bs = BeautifulSoup(response.text, 'html.parser')
             self._extract_url(article_bs)
 
@@ -73,7 +74,7 @@ class HTMLParser:
         self.article = Article(article_url, article_id)
 
     def parse(self):
-        response = requests.get(self.article_url, headers=HEADERS)
+        response = requests.get(self.article_url, headers=headers)
         article_bs = BeautifulSoup(response.text, 'html.parser')
         self._fill_article_with_text(article_bs)
         self._fill_article_with_meta_information(article_bs)
@@ -82,7 +83,7 @@ class HTMLParser:
     def _fill_article_with_text(self, article_bs):
         fulltext = article_bs.find('ul', class_='value galleys_links')
         page_link = fulltext.find('a')['href']
-        response_pdf = requests.get(page_link, headers=HEADERS)
+        response_pdf = requests.get(page_link, headers=headers)
         pdf_bs = BeautifulSoup(response_pdf.text, 'html.parser')
         container = pdf_bs.find('header', class_='header_view')
         download_link = container.find('a', class_='download')['href']
