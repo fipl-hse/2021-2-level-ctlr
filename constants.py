59285d2c32ccf019b84e8e312ef719aa1dab8d47
Vasilisa-Blyudova <90454966+Vasilisa-Blyudova@users.noreply.github.com>
2022-03-23 21:33:05 +0300

    again style

--- constants.py
+++ constants.py
@@ -8,7 +8,7 @@ PROJECT_ROOT = Path(__file__).parent
 ASSETS_PATH = PROJECT_ROOT / 'tmp' / 'articles'
 CRAWLER_CONFIG_PATH = PROJECT_ROOT / 'scrapper_config.json'
 HEADERS = {}
-USER_AGENT = '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) 
+USER_AGENT = '''Mozilla/5.0 (Windows NT 10.0; Win64; x64)
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36'''
 HEADERS['user-agent'] = USER_AGENT
 URL = 'https://journals.vsu.ru'
