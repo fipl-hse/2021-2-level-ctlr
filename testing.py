3879719327f763b45736e88baf722dec31562459
Vlados13iq <90451686+Vlados13iq@users.noreply.github.com>
2022-03-23 22:39:42 +0300

    Delete testing.py

--- testing.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-try:
-    html_file = requests.get('https://snob.ru/theme/545/').text
-    with open('test.html', 'w', encoding='utf-8') as f:
-        f.write(html_file)
-except NameError:
-    print('Website does not exist')
