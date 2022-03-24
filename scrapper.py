68120a3f1597080b4ed4c0579881aa8958ca8bce
DashaKrinochkina <90447363+DashaKrinochkina@users.noreply.github.com>
2022-03-24 19:03:12 +0300

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -9,6 +9,7 @@ import datetime
 from time import sleep
 import requests
 from bs4 import BeautifulSoup
+# checks
 
 from core_utils.article import Article
 from core_utils.pdf_utils import PDFRawFile
