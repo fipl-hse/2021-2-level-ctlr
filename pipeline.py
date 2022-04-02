f09fcd8a85893b4b9b2b68844d1058d0171d7af7
tatarinovst2 <SpaceUltraMax@yandex.ru>
2022-04-02 21:00:31 +0300

    Started working on pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,14 @@
 """
 Pipeline for text processing implementation
 """
+import pathlib
+import re
+
+import pymorphy2
+from pymystem3 import Mystem
+
+from constants import ASSETS_PATH
+from core_utils.article import Article
 
 
 class EmptyDirectoryError(Exception):
@@ -21,25 +29,28 @@ class MorphologicalToken:
     """
 
     def __init__(self, original_word):
-        pass
+        self.original_word = original_word
+        self.normalized_form = ''
+        self.tags_mystem = ''
+        self.tags_pymorphy = ''
 
     def get_cleaned(self):
         """
         Returns lowercased original form of a token
         """
-        pass
+        return self.original_word.lower()
 
     def get_single_tagged(self):
         """
         Returns normalized lemma with MyStem tags
         """
-        pass
+        return f'{self.normalized_form}<{self.tags_mystem}>'
 
     def get_multiple_tagged(self):
         """
         Returns normalized lemma with PyMorphy tags
         """
-        pass
+        return f'{self.normalized_form}<{self.tags_mystem}>({self.tags_pymorphy})'
 
 
 class CorpusManager:
@@ -48,19 +59,32 @@ class CorpusManager:
     """
 
     def __init__(self, path_to_raw_txt_data: str):
-        pass
+        self.path_to_raw_txt_data = path_to_raw_txt_data
+        self._storage = {}
+        self._scan_dataset()
 
     def _scan_dataset(self):
         """
         Register each dataset entry
         """
-        pass
+        pathlib_path_to_data = pathlib.Path(self.path_to_raw_txt_data)
+
+        for full_file_name in pathlib_path_to_data.iterdir():
+            file_name = full_file_name.name
+
+            if file_name[-8:] == "_raw.txt":
+                match = re.search(r'\d+', file_name)
+                article_id = int(match.group(0))
+
+                article = Article(url=None, article_id=article_id)
+
+                self._storage[article_id] = article
 
     def get_articles(self):
         """
         Returns storage params
         """
-        pass
+        return self._storage
 
 
 class TextProcessingPipeline:
@@ -69,31 +93,122 @@ class TextProcessingPipeline:
     """
 
     def __init__(self, corpus_manager: CorpusManager):
-        pass
+        self.corpus_manager = corpus_manager
 
     def run(self):
         """
         Runs pipeline process scenario
         """
-        pass
+        articles = self.corpus_manager.get_articles().values()
+
+        for article in articles:
+            raw_text = article.get_raw_text()
+            processed_tokens = self._process(raw_text)
+
+            cleaned_tokens = []
+            single_tagged_tokens = []
+            multiple_tagged_tokens = []
+
+            for processed_token in processed_tokens:
+                cleaned_tokens.append(processed_token.get_cleaned())
+                single_tagged_tokens.append(processed_token.get_single_tagged())
+                multiple_tagged_tokens.append(processed_token.get_multiple_tagged())
+
+            article.save_as(' '.join(cleaned_tokens), 'cleaned')
+            article.save_as(' '.join(single_tagged_tokens), 'single_tagged')
+            article.save_as(' '.join(multiple_tagged_tokens), 'multiple_tagged')
 
     def _process(self, raw_text: str):
         """
         Processes each token and creates MorphToken class instance
         """
-        pass
+        # Gets rid of '-\n's that break up words, so we won't get 'кот-' 'орый' as example (Should be: 'который')
+        text = raw_text.replace('-\n', '')
+
+        result = Mystem().analyze(text)
+
+        morphological_tokens = []
+
+        for token_info in result:
+            original_word = token_info['text']
+
+            if not re.match(r'[а-яА-Яa-zA-Z]', original_word):
+                continue
+
+            morphological_token = MorphologicalToken(original_word=original_word)
+
+            if 'analysis' not in token_info:
+                continue
+
+            if not token_info['analysis']:
+                continue
+
+            morphological_token.normalized_form = token_info['analysis'][0]['lex']
+            morphological_token.tags_mystem = token_info['analysis'][0]['gr']
+
+            morph = pymorphy2.MorphAnalyzer()
+            p = morph.parse(original_word)[0]
+            morphological_token.tags_pymorphy = p.tag
+
+            morphological_tokens.append(morphological_token)
+
+        return morphological_tokens
 
 
 def validate_dataset(path_to_validate):
     """
     Validates folder with assets
     """
-    pass
+
+    pathlib_path_to_validate = pathlib.Path(path_to_validate)
+
+    if not path_to_validate.exists():
+        raise FileNotFoundError
+
+    if not pathlib_path_to_validate.is_dir():
+        raise NotADirectoryError
+
+    file_ids = []
+
+    for full_file_name in pathlib_path_to_validate.iterdir():
+        file_name = full_file_name.name
+
+        # In case macOS Finder system file exists (Ignore this)
+        if file_name == '.DS_Store':
+            full_file_name.unlink()
+            continue
+
+        match = re.match(r'\d+', file_name)
+
+        if not match:
+            raise InconsistentDatasetError
+
+        file_id = int(match.group(0))
+
+        file_ids.append(file_id)
+
+    file_ids = sorted(file_ids)
+
+    last_file_id = 0
+
+    for file_id in file_ids:
+        # if not last_file_id and file_id != 1:
+            # raise InconsistentDatasetError
+
+        if not 0 <= file_id - last_file_id <= 1:
+            raise InconsistentDatasetError
+
+        last_file_id = file_id
+
+    if not last_file_id:
+        raise EmptyDirectoryError
 
 
 def main():
-    # YOUR CODE HERE
-    pass
+    validate_dataset(ASSETS_PATH)
+    corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
+    pipeline = TextProcessingPipeline(corpus_manager=corpus_manager)
+    pipeline.run()
 
 
 if __name__ == "__main__":
--- pos_frequency_pipeline.py
+++ pos_frequency_pipeline.py
@@ -1,6 +1,12 @@
 """
 Implementation of POSFrequencyPipeline for score ten only.
 """
+import json
+import re
+
+from constants import ASSETS_PATH
+from pipeline import CorpusManager
+from core_utils.visualizer import visualize
 
 
 class EmptyFileError(Exception):
@@ -10,16 +16,48 @@ class EmptyFileError(Exception):
 
 
 class POSFrequencyPipeline:
-    def __init__(self, corpus):
-        pass
+    def __init__(self, corpus_manager):
+        self.corpus_manager = corpus_manager
 
     def run(self):
-        pass
+        pos_frequencies = {}
+
+        for article in self.corpus_manager.get_articles().values():
+            article_path = article.get_file_path('single_tagged')
+
+            with open(article_path, 'r') as text_file:
+                text = text_file.read()
+
+                if not text:
+                    raise EmptyFileError
+
+                tokens = text.split()
+
+                for token in tokens:
+                    match = re.search(r'<[A-Z]+', token)
+
+                    if not match:
+                        continue
+
+                    pos = match.group(0)[1:]
+                    pos_frequencies[pos] = pos_frequencies.get(pos, 0) + 1
+
+            with open(ASSETS_PATH / article.get_meta_file_path(), 'r', encoding='utf-8') as meta_file:
+                meta_data = json.load(meta_file)
+                meta_data.update({'pos_frequencies': pos_frequencies})
+
+            with open(ASSETS_PATH / article.get_meta_file_path(), 'w', encoding='utf-8') as meta_file:
+                json.dump(meta_data, meta_file, sort_keys=False,
+                          indent=4, ensure_ascii=False, separators=(',', ': '))
+
+            visualize(statistics=pos_frequencies, path_to_save=ASSETS_PATH / f'{article.article_id}_image.png')
 
 
 def main():
-    # YOUR CODE HERE
-    pass
+    corpus_manager = CorpusManager(ASSETS_PATH)
+
+    pipeline = POSFrequencyPipeline(corpus_manager=corpus_manager)
+    pipeline.run()
 
 
 if __name__ == "__main__":
