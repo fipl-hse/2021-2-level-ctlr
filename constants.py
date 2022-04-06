d043da186e406c576a17db006796411ce471edb5
Anastasia <awake777.20@gmail.com>
2022-04-06 20:27:45 +0300

    If there's smth wrong again I'll start singing smth else

--- constants.py
+++ constants.py
@@ -8,7 +8,7 @@ PROJECT_ROOT = Path(__file__).parent
 ASSETS_PATH = PROJECT_ROOT / 'tmp' / 'articles'
 CRAWLER_CONFIG_PATH = PROJECT_ROOT / 'scrapper_config.json'
 HEADERS = {
-            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N)' 
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N)'
             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.119 Mobile Safari/537.36',
             'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'ru,en;q=0.9,de;q=0.8',
