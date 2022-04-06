b454abbb0ed0bcb943577daf94b22e1b999c7c32
eapareshina <zhenya.pareshina@gmail.com>
2022-04-06 18:58:52 +0300

    some problems are solved but there are still some problems to fix

--- scrapper.py
+++ scrapper.py
@@ -1,16 +1,20 @@
 """
 Scrapper implementation
 """
+from datetime import datetime
 import json
-import os
+from pathlib import Path
+import random
 import re
+import shutil
 from time import sleep
 
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-from constants import ASSETS_PATH, CRAWLER_CONFIG_PATH, HEADERS
+from constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
 from core_utils.article import Article
+from core_utils.pdf_utils import PDFRawFile
 
 
 class IncorrectURLError(Exception):
@@ -42,18 +46,19 @@ class Crawler:
         self.urls = []
 
     def _extract_url(self, article_bs):
-        article_summaries = article_bs.find("div", class_="obj_article_summary")
-        articles = article_summaries.find("div", class_="title")
-        for link in articles:
-            self.urls.append(link.find('a')['href'])
+        article_summaries_bs = article_bs.find_all("div", class_="obj_article_summary")
+        urls_bs = [article_summary_bs.find('div', class_='title').find(
+            'a')['href'] for article_summary_bs in article_summaries_bs]
+        self.urls.extend(urls_bs)
 
     def find_articles(self):
         """
         Finds articles
         """
-        sleep_period = 5
+
         for url in self.seed_urls:
-            response = requests.get(url, headers=HEADERS)
+            response = requests.get(url)
+            sleep_period = random.randint(1, 3)
             sleep(sleep_period)
 
             if not response.ok:
@@ -62,7 +67,6 @@ class Crawler:
             soup = BeautifulSoup(response.text, 'lxml')
             self._extract_url(soup)
 
-
     def get_search_urls(self):
         """
         Returns seed_urls param
@@ -70,49 +74,80 @@ class Crawler:
         return self.seed_urls
 
 
-class ArticleParser:
+class HTMLParser:
     def __init__(self, article_url, article_id):
         self.article_url = article_url
         self.article_id = article_id
         self.article = Article(article_url, article_id)
 
-    def parse(self):
-        response = requests.get(self.article_url, headers=HEADERS)
-        with open(f'{ASSETS_PATH}/{self.article_id}_article.html', 'w') as file:
-            file.write(response.text)
-
     def _fill_article_with_text(self, article_bs):
-        return None
+        article_urls_bs = article_bs.find('a', class_='obj_galley_link pdf')['href']
+        response = requests.get(article_urls_bs)
+        soup = BeautifulSoup(response.text, 'lxml')
+        url_download_pdf = soup.find('a', class_='download')['href']
+        pdf = PDFRawFile(url_download_pdf, self.article_id)
+        pdf.download()
 
+        self.article.text = pdf.get_text().split('СПИСОК ЛИТЕРАТУРЫ')[0]
 
-# class PDFCrawler(HTMLCrawler):
-#     pass
+    def _fill_article_with_meta_information(self, article_bs):
+        # title
+        self.article.title = article_bs.find('h1', class_='page_title').text.strip()
+
+        # author
+        self.article.author = article_bs.find('ul', class_='item authors').find('li').find('span').text.strip()
+
+        # topics
+        self.article.topics = article_bs.find('div', class_='item keywords').find(
+            'span', class_='value').text.strip().replace('\t', "").split(', ')
+
+        # date
+        try:
+            date = article_bs.find('div', class_='item published').find('div', class_='value').text.strip()
+            self.article.date = datetime.strptime(date, '%Y-%m-%d')
+        except AttributeError:
+            self.article.date = datetime.strptime('2021', '%Y')
+
+    def parse(self):
+        response = requests.get(url=self.article_url)
+
+        article_bs = BeautifulSoup(response.text, 'lxml')
+
+        self._fill_article_with_text(article_bs)
+        self._fill_article_with_meta_information(article_bs)
+        return self.article
 
 
 def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    if ASSETS_PATH.exists():
-        ASSETS_PATH.unlink()
-    try:
-        os.removedirs(ASSETS_PATH)
-    except FileNotFoundError:
-        pass
-    os.makedirs(ASSETS_PATH)
+
+    as_path = Path(base_path)
+    if as_path.exists():
+        shutil.rmtree(as_path)
+    as_path.mkdir(parents=True)
 
 
 def validate_config(crawler_path):
     """
     Validates given config
     """
-    with open(crawler_path) as file:
+    with open(crawler_path, 'r', encoding='utf-8') as file:
         config = json.load(file)
+
+    pattern = re.compile(r"https?://journal\.asu\.ru/urisl/issue/.+")
+
+    for url in config["seed_urls"]:
+        result = pattern.search(url)
+        if not result:
+            raise IncorrectURLError
 
     seed_urls = config["seed_urls"]
     total_articles = config['total_articles_to_find_and_parse']
 
-    pattern = re.compile(r"^https?://")
+    if not seed_urls:
+        raise IncorrectURLError
 
     if not isinstance(total_articles, int):
         raise IncorrectNumberOfArticlesError
@@ -120,22 +155,30 @@ def validate_config(crawler_path):
     if total_articles > 200:
         raise NumberOfArticlesOutOfRangeError
 
-    for url in seed_urls:
-        if not pattern.search(url):
-            raise IncorrectURLError
+    if total_articles <= 0:
+        raise IncorrectNumberOfArticlesError
 
     return seed_urls, total_articles
 
 
 if __name__ == '__main__':
-    # with open(CRAWLER_CONFIG_PATH) as file:
-    #     config = json.load(file)
-    #
-    # seed_urls = config['seed_urls']
-    # max_articles = config['total_articles']
-    # crawler = Crawler(seed_urls=seed_urls, total_max_articles=max_articles)
-    #
-    # crawler.find_articles()
-    #
-    # parser = ArticleParser(article_url=full_url, article_id=i)
+    print('preparing environment')
+    new_seed_urls, new_total_articles = validate_config(CRAWLER_CONFIG_PATH)
     prepare_environment(ASSETS_PATH)
+    print('creating Crawler')
+    crawler = Crawler(new_seed_urls, new_total_articles)
+    crawler.find_articles()
+    print('parsing')
+    ID_OF_ARTICLE = 1
+    for article_url in crawler.urls:
+        article_parser = HTMLParser(article_url=article_url, article_id=ID_OF_ARTICLE)
+        try:
+            article = article_parser.parse()
+            article.save_raw()
+            print(f'the {ID_OF_ARTICLE} article is successfully downloaded')
+            ID_OF_ARTICLE += 1
+        except Exception as e:
+            print(e)
+            print(article_url)
+
+    print("That's all!")
