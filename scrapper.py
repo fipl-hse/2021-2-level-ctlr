019466fccd9d2a830ffb20930041ee5be851cf20
danalifshits <danessia03@gmail.com>
2022-04-15 11:06:37 +0300

    final version (i hope 10)

--- scrapper.py
+++ scrapper.py
@@ -64,7 +64,7 @@ class Crawler:
             sleep(random.randint(1, 5))
             response = requests.get(url=seed_url, headers={
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36'
-                              '(KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36'
+                            '(KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36'
             })
             if not response.ok:
                 continue
