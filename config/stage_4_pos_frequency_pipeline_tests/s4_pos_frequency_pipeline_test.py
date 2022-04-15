beb4b5a29eb116e16a00cf9bbb11dbba750d1d77
dmitry-uraev <uraev.dima@yandex.ru>
2022-04-15 10:34:13 +0300

    Skip

--- config/stage_4_pos_frequency_pipeline_tests/s4_pos_frequency_pipeline_test.py
+++ config/stage_4_pos_frequency_pipeline_tests/s4_pos_frequency_pipeline_test.py
@@ -1,3 +1,4 @@
+# pylint: skip-file
 """
 Tests for POS frequency pipeline
 """
--- scrapper.py
+++ scrapper.py
@@ -26,7 +26,7 @@ class Crawler:
     Crawler implementation
     """
     def __init__(self, seed_urls, max_articles: int):
-        self.urls = seed_urls
+        pass
 
     def _extract_url(self, article_bs):
         pass
@@ -40,20 +40,6 @@ class Crawler:
     def get_search_urls(self):
         """
         Returns seed_urls param
-        """
-        pass
-
-
-class HTMLParser:
-    """
-    ArticleParser implementation
-    """
-    def __init__(self, full_url: str, article_id: int):
-        self.article = Article(full_url, self.article_id)
-
-    def parse(self):
-        """
-        Parses each article
         """
         pass
 
