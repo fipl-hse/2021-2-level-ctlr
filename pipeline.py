5b1cd1410ce33a3ebad7f8c7e7dda681cc6631f8
DashaKrinochkina <dasha060402@mail.ru>
2022-04-19 22:48:33 +0300

    added functions for 4

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,10 @@
 """
 Pipeline for text processing implementation
 """
+from core_utils.article import Article, ArtifactType
 from pathlib import Path
+import re
+# from pymystem3 import Mystem
 
 
 class EmptyDirectoryError(Exception):
@@ -25,13 +28,16 @@ class MorphologicalToken:
     """
 
     def __init__(self, original_word):
-        pass
+        self.original_word = original_word
+        self.normalized_form = ''
+        self.tags_mystem = ''
+        self.tags_pymorphy = ''
 
     def get_cleaned(self):
         """
         Returns lowercased original form of a token
         """
-        pass
+        return self.original_word.lower()
 
     def get_single_tagged(self):
         """
@@ -52,19 +58,26 @@ class CorpusManager:
     """
 
     def __init__(self, path_to_raw_txt_data: str):
-        pass
+        self._storage = {}
+        self.path = Path(path_to_raw_txt_data)
 
     def _scan_dataset(self):
         """
         Register each dataset entry
         """
-        pass
+        children = [child for child in self.path.glob('*')]
+
+        for child in children:
+            if 'raw.txt' in str(child):
+                article_id = re.search(r'[0-9]+_raw', str(child)).group(0)[:-4]
+                self._storage[int(article_id)] = Article(url=None, article_id=int(article_id))
 
     def get_articles(self):
         """
         Returns storage params
         """
-        pass
+        self._scan_dataset()
+        return self._storage
 
 
 class TextProcessingPipeline:
@@ -73,19 +86,57 @@ class TextProcessingPipeline:
     """
 
     def __init__(self, corpus_manager: CorpusManager):
-        pass
+        self.corpus_manager = corpus_manager
 
     def run(self):
         """
         Runs pipeline process scenario
         """
-        pass
+        articles = self.corpus_manager.get_articles().values()
+        for article in articles:
+            raw_text = article.get_raw_text()
+            processed_text = self._process(raw_text)
+
+            cleaned_tokens = []
+            for token in processed_text:
+                cleaned_tokens.append(token.get_cleaned())
+
+            article.save_as(' '.join(cleaned_tokens), ArtifactType.cleaned)
 
     def _process(self, raw_text: str):
         """
         Processes each token and creates MorphToken class instance
         """
-        pass
+        clean_text = ''
+        for symbol in raw_text:
+            if symbol.isalpha():
+                clean_text += symbol
+            if symbol.isspace() or symbol == '\n':
+                clean_text += ' '
+            if symbol == '-\n':
+                clean_text += ''
+        clean_list = clean_text.split()
+
+        morph_tokens = []
+        for word in clean_list:
+            token = MorphologicalToken(original_word=word)
+            morph_tokens.append(token)
+
+        '''
+        raw_text = raw_text.replace('-\n', '')
+        result = Mystem().analyze(raw_text)
+        morph_tokens = []
+
+        for element in result:
+            word = element['text']
+            if not word.isalpha():
+                continue
+
+            token = MorphologicalToken(original_word=word)
+            morph_tokens.append(token)
+        '''
+
+        return morph_tokens
 
 
 def validate_dataset(path_to_validate):
@@ -106,7 +157,7 @@ def validate_dataset(path_to_validate):
         raise EmptyDirectoryError
 
     for child in children:
-        if 'txt' in str(child):
+        if 'raw.txt' in str(child):
             with open(child, 'r', encoding='utf-8') as file:
                 text = file.read()
                 if not text:
@@ -117,7 +168,7 @@ def validate_dataset(path_to_validate):
     for file_name in children:
         if 'meta' in str(file_name):
             counter_m += 1
-        elif 'txt' in str(file_name):
+        elif 'raw.txt' in str(file_name):
             counter_t += 1
 
     if counter_t != counter_m:
