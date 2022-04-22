989663f36a0a8d8b8584d2fe164fc0a43bfcc976
Alexander Demidovskij <monadv@yandex.ru>
2022-04-22 09:30:24 +0300

    new practice

---
+++ seminars/04.22.2022/try_re.py
@@ -0,0 +1,12 @@
+# Task 1. Extract host from the given log
+# Task 2. Extract username from the given log
+# Task 3. Extract request as string from the given log
+# Task 4. Extract time as string from the given log
+# Task 5. Extract time as datetime object from the given log
+# Task 6. Wrap log analysis as a function
+
+def main():
+    print('Hello, World!')
+
+if __name__ == '__main__':
+    main()
