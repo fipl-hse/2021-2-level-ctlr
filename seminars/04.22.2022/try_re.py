9f19738b4118feb32d4dc8801ad28f5016bcd940
Alexander Demidovskij <monadv@yandex.ru>
2022-04-22 09:31:55 +0300

    new practice #2

--- seminars/04.22.2022/try_re.py
+++ seminars/04.22.2022/try_re.py
@@ -3,10 +3,11 @@
 # Task 3. Extract request as string from the given log
 # Task 4. Extract time as string from the given log
 # Task 5. Extract time as datetime object from the given log
-# Task 6. Wrap log analysis as a function
+# Task 6. Wrap log analysis as a function and find out how many entrances (should be 979)
 
 def main():
     print('Hello, World!')
+
 
 if __name__ == '__main__':
     main()
