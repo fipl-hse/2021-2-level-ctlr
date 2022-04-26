fc7c78a83385da9f429201eedaac1aad1e6d7335
DashaKrinochkina <dasha060402@mail.ru>
2022-04-26 13:07:09 +0300

    .....

--- constants.py
+++ constants.py
@@ -11,4 +11,12 @@ HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/5
                          '(KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
            'accept': '*/*',
            'accept-encoding': 'gzip, deflate, br',
-           'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'}
+           'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+           'cookie': 'yandexuid=5687879911644746701; yuidss=5687879911644746701; '
+                     'ymex=1960108130.yrts.1644748130#1960108129.yrtsi.1644748129; '
+                     'is_gdpr=0; _ym_uid=1645129214159782619; is_gdpr_b=CPrlYRD4ZSgC; '
+                     '_ym_d=1646573834; my=YwA=; i=O/LSyUkDVItvP6mrxwFvhLiSwuukrl5QrxYX'
+                     'rjVo+z2gpcFOFTc3Yj4ZSE7HnZvC9yiHvr7pgMQHPgPx0SzsMwhMBoE=; '
+                     'skid=3746917891646583925; yabs-frequency=/5/0000000000000000/Oqhql'
+                     '5GgddXyHM3QagJCGnBgSdn58Fzy-QAONRbNV4KX5Hn1ROO0001yHIE2OK5jXW0007n58'
+                     'm00/; yabs-sid=2303709261650966773'}
