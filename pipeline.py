095f16ffda62799812879efb8c4feb77ea2a5e19
Lyubov Talanina <72165770+lyutl@users.noreply.github.com>
2022-04-26 18:31:33 +0300

    trying...

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,11 @@ Pipeline for text processing implementation
 
 from pathlib import Path
 
-from core_utils.article import Article
+import pymorphy2
+from pymystem3 import Mystem
+
+from constants import ASSETS_PATH
+from core_utils.article import Article, ArtifactType
 
 
 class EmptyDirectoryError(Exception):
@@ -28,25 +32,28 @@ class MorphologicalToken:
     """
 
     def __init__(self, original_word):
-        pass
+        self.original_word = original_word
+        self.normalized_form = ''
+        self.tags_mystem = ''
+        self.tags_pymorphy = ''
 
     def get_cleaned(self):
         """
         Returns lowercased original form of a token
         """
-        pass
+        return self.original_word.lower()
 
     def get_single_tagged(self):
         """
         Returns normalized lemma with MyStem tags
         """
-        pass
+        return f'{self.normalized_form}<{self.tags_mystem}>'
 
     def get_multiple_tagged(self):
         """
         Returns normalized lemma with PyMorphy tags
         """
-        pass
+        return f'{self.normalized_form}<{self.tags_mystem}>({self.tags_pymorphy})'
 
 
 class CorpusManager:
@@ -84,26 +91,69 @@ class TextProcessingPipeline:
     """
 
     def __init__(self, corpus_manager: CorpusManager):
-        pass
+        self.corpus_manager = corpus_manager
 
     def run(self):
         """
         Runs pipeline process scenario
         """
-        pass
+        articles = CorpusManager.get_articles(self.corpus_manager).values()
+
+        for article in articles:
+            raw_text = Article.get_raw_text(article)
+            tokens = self._process(raw_text)
+
+            cleaned = []
+            single_tagged = []
+            multiple_tagged = []
+            for token in tokens:
+                cleaned.append(token.get_cleaned())
+                single_tagged.append(token.get_single_tagged())
+                multiple_tagged.append(token.get_multiple_tagged())
+
+            article.save_as(' '.join(cleaned), ArtifactType.cleaned)
+            article.save_as(' '.join(single_tagged), ArtifactType.single_tagged)
+            article.save_as(' '.join(multiple_tagged), ArtifactType.multiple_tagged)
 
     def _process(self, raw_text: str):
         """
         Processes each token and creates MorphToken class instance
         """
-        pass
+        clean_text = ''
+
+        for symbol in raw_text.replace('-\n', ''):
+            if symbol.isalpha():
+                clean_text += symbol
+            if symbol.isspace() or symbol == '\n':
+                clean_text += ' '
+
+        mystem = Mystem()
+        morph_analyzer = pymorphy2.MorphAnalyzer()
+
+        clean_text_analysis = mystem.analyze(clean_text)
+
+        tokens = []
+        for single_word_analysis in clean_text_analysis:
+            if single_word_analysis['text'].isalpha() and single_word_analysis['analysis']:
+                token = MorphologicalToken(single_word_analysis['text'])
+                tokens.append(token)
+
+                analysis = single_word_analysis['analysis']
+                token.normalized_form = analysis[0]['lex']
+                token.tags_mystem = analysis[0]['gr']
+
+                parses = morph_analyzer.parse(single_word_analysis['text'])
+                token.tags_pymorphy = parses[0].tag
+
+        return tokens
 
 
 def validate_dataset(path_to_validate):
     """
     Validates folder with assets
     """
-    path_to_validate = Path(path_to_validate)
+    if isinstance(path_to_validate, str):
+        path_to_validate = Path(path_to_validate)
 
     files = [file for file in path_to_validate.glob('*')]
     stems = [file.stem for file in files]
@@ -120,13 +170,18 @@ def validate_dataset(path_to_validate):
         raise EmptyDirectoryError
 
     for file in files:
+        if file.stat().st_size == 0:
+            raise InconsistentDatasetError
+
         if file.suffix == '.json':
+
             meta_json += 1
 
             if f'{meta_json}_meta' not in stems:
                 raise InconsistentDatasetError
 
         if file.suffix == '.txt':
+
             raw_txt += 1
 
             if f'{raw_txt}_raw' not in stems:
@@ -137,8 +192,10 @@ def validate_dataset(path_to_validate):
 
 
 def main():
-    # YOUR CODE HERE
-    pass
+    validate_dataset(ASSETS_PATH)
+    corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
+    pipeline = TextProcessingPipeline(corpus_manager=corpus_manager)
+    pipeline.run()
 
 
 if __name__ == "__main__":
