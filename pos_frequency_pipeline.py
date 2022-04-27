7f9b3587349322de8d4f4accb5c5b5730b961a9e
KarinaZakirova <90447262+KarinaZakirova@users.noreply.github.com>
2022-04-27 12:02:21 +0300

    maybe it is right 2.0

--- pos_frequency_pipeline.py
+++ pos_frequency_pipeline.py
@@ -1,8 +1,13 @@
 """
 Implementation of POSFrequencyPipeline for score ten only.
 """
+import json
+import re
 
-from pipeline import CorpusManager
+from constants import ASSETS_PATH
+from core_utils.article import ArtifactType
+from core_utils.visualizer import visualize
+from pipeline import CorpusManager, validate_dataset
 
 
 class EmptyFileError(Exception):
@@ -13,15 +18,53 @@ class EmptyFileError(Exception):
 
 class POSFrequencyPipeline:
     def __init__(self, corpus_manager: CorpusManager):
-        pass
+        self.corpus_manager = corpus_manager
 
     def run(self):
-        pass
+        """
+        Runs pipeline process scenario:
+        iterates through the available articles taken from CorpusManager,
+        reads each file,
+        calculates frequencies of each part of speech,
+        writes them to the meta file
+        visualizes them in a form of images with names following this convention: N_image.png.
+        """
+
+        for article in self.corpus_manager.get_articles().values():
+
+            with open(article.get_file_path(ArtifactType.single_tagged), encoding='utf=8') as file:
+                file_text = file.read()
+                if not file_text:
+                    raise EmptyFileError
+
+            # calculate frequencies of each part of speech
+            freq_dict = {}
+            pattern = re.compile(r'<([A-Z]+)')
+
+            for pos_tag in pattern.findall(file_text):
+                freq_dict[pos_tag] = freq_dict.get(pos_tag, 0) + 1
+
+            # write frequencies to the meta file
+            with open(ASSETS_PATH / article.get_meta_file_path(), encoding='utf-8') as meta_f:
+                meta_data = json.load(meta_f)
+
+            meta_data.update({'pos_frequencies': freq_dict})
+
+            with open(ASSETS_PATH / article.get_meta_file_path(), 'w', encoding='utf-8') as meta_f:
+                json.dump(meta_data, meta_f, sort_keys=False, indent=4,
+                          ensure_ascii=False, separators=(',', ':'))
+
+            # visualize frequencies in a form of images with names N_image.png.
+            visualize(statistics=freq_dict,
+                      path_to_save=ASSETS_PATH / f'{article.article_id}_image.png')
 
 
 def main():
     # YOUR CODE HERE
-    pass
+    validate_dataset(ASSETS_PATH)
+    corpus_manager = CorpusManager(ASSETS_PATH)
+    pipeline = POSFrequencyPipeline(corpus_manager=corpus_manager)
+    pipeline.run()
 
 
 if __name__ == "__main__":
