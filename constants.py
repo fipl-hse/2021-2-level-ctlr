f32d6443db0610ad5fee60476a056dc8c356bbb5
DashaKrinochkina <90447363+DashaKrinochkina@users.noreply.github.com>
2022-04-28 01:23:35 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -7,11 +7,9 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).parent
 ASSETS_PATH = PROJECT_ROOT / 'tmp' / 'articles'
 CRAWLER_CONFIG_PATH = PROJECT_ROOT / 'scrapper_config.json'
-'''
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-                         '(KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.36',
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'
-                      'image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-           'Accept-Encoding': 'gzip, deflate',
-           'Accept-Language': 'ru,en-GB;q=0.9,en;q=0.8,en-US;q=0.7'}
-'''
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 '
+           '(KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
+           'Accept': '*/*',
+           'Accept-Encoding': 'gzip, deflate, br',
+           'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'}
