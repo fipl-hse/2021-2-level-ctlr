b0396fe4580c5c907c2f18629ebd6e6c5d040899
DashaKrinochkina <90447363+DashaKrinochkina@users.noreply.github.com>
2022-04-27 20:44:10 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -12,11 +12,16 @@ HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/5
            'accept': '*/*',
            'accept-encoding': 'gzip, deflate, br',
            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+           'sec-ch-ua': " Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100",
+           'sec-ch-ua-mobile': '?0',
+           'sec-ch-ua-platform': "Windows",
+           'sec-fetch-dest': 'empty',
+           'sec-fetch-mode': 'cors',
+           'sec-fetch-site': 'cross-site',
            'cookie': 'yandexuid=5687879911644746701; yuidss=5687879911644746701; '
-                     'ymex=1960108130.yrts.1644748130#1960108129.yrtsi.1644748129; '
-                     'is_gdpr=0; _ym_uid=1645129214159782619; is_gdpr_b=CPrlYRD4ZSgC; '
-                     '_ym_d=1646573834; my=YwA=; i=O/LSyUkDVItvP6mrxwFvhLiSwuukrl5QrxYX'
-                     'rjVo+z2gpcFOFTc3Yj4ZSE7HnZvC9yiHvr7pgMQHPgPx0SzsMwhMBoE=; '
-                     'skid=3746917891646583925; yabs-frequency=/5/0000000000000000/Oqhql'
-                     '5GgddXyHM3QagJCGnBgSdn58Fzy-QAONRbNV4KX5Hn1ROO0001yHIE2OK5jXW0007n58'
-                     'm00/; yabs-sid=2303709261650966773'}
+           'ymex=1960108130.yrts.1644748130#1960108129.yrtsi.1644748129; '
+           'is_gdpr=0; _ym_uid=1645129214159782619; is_gdpr_b=CPrlYRD4ZSgC; '
+           'my=YwA=; yandex_gid=47; _ym_d=1650977028; MGphYZof=1; computer=1; '
+           'yabs-frequency=/5/0000000000000000/teR6PWWKsNz8HY5wlnSwdWwCTaX68RMKMpnRSMDS'
+           'I4OZ8krF10oancv8HYDZIlIyL2gUU4X6WDgIfCn34kfoI4OW/; i=SyudZhTUgwGo4+aw44K2ho/vDU6zcg'
+           'sazAkrcqDNh2YeB9K6HAa6xdtqD5CLFLY0JdVHajOmsCRfbHh+KYq0NzFreWQ=; yabs-sid=2627592021651080827'}
