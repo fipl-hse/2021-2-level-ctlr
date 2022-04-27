7df98aa75304f3f979fbd2e847ccab92d643678b
Kate <kate2002elisey@gmail.com>
2022-04-27 13:24:56 +0300

    i cant do it anymore3

--- constants.py
+++ constants.py
@@ -8,5 +8,9 @@ PROJECT_ROOT = Path(__file__).parent
 ASSETS_PATH = PROJECT_ROOT / 'tmp' / 'articles'
 CRAWLER_CONFIG_PATH = PROJECT_ROOT / 'scrapper_config.json'
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-                         '(KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
+                         '(KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+           'accept-encoding': 'gzip, deflate, br',
+           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,'
+                     'image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+           'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'}
 URL_PATTERN = "https://www.politlinguistika.ru"
