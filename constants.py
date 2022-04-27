05e134a77f6edacb2877970fb6ec8d893af7a4f7
DashaKrinochkina <dasha060402@mail.ru>
2022-04-27 20:58:20 +0300

    headers

--- constants.py
+++ constants.py
@@ -12,7 +12,7 @@ HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/5
            'accept': '*/*',
            'accept-encoding': 'gzip, deflate, br',
            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-           'sec-ch-ua': " Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100",
+           'sec-ch-ua': '''" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"''',
            'sec-ch-ua-mobile': '?0',
            'sec-ch-ua-platform': "Windows",
            'sec-fetch-dest': 'empty',
@@ -20,8 +20,13 @@ HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/5
            'sec-fetch-site': 'cross-site',
            'cookie': 'yandexuid=5687879911644746701; yuidss=5687879911644746701; '
                      'ymex=1960108130.yrts.1644748130#1960108129.yrtsi.1644748129; '
-                     'is_gdpr=0; _ym_uid=1645129214159782619; is_gdpr_b=CPrlYRD4ZSgC; '
-                     'my=YwA=; yandex_gid=47; _ym_d=1650977028; MGphYZof=1; computer=1; '
+                     'is_gdpr=0; _ym_uid=1645129214159782619; '
+                     'is_gdpr_b=CPrlYRD4ZSgC; '
+                     'my=YwA=; yandex_gid=47; '
+                     '_ym_d=1650977028; '
+                     'MGphYZof=1; '
+                     'computer=1; '
                      'yabs-frequency=/5/0000000000000000/teR6PWWKsNz8HY5wlnSwdWwCTaX68RMKMpnRSMDS'
-                     'I4OZ8krF10oancv8HYDZIlIyL2gUU4X6WDgIfCn34kfoI4OW/; i=SyudZhTUgwGo4+aw44K2ho/vDU6zcg'
-                     'sazAkrcqDNh2YeB9K6HAa6xdtqD5CLFLY0JdVHajOmsCRfbHh+KYq0NzFreWQ=; yabs-sid=2627592021651080827'}
+                     'I4OZ8krF10oancv8HYDZIlIyL2gUU4X6WDgIfCn34kfoI4OW/; '
+                     'i=SyudZhTUgwGo4+aw44K2ho/vDU6zcgsazAkrcqDNh2YeB9K6HAa6xdtqD5CLFLY0JdVHajOmsCRfbHh+KYq0NzFreWQ=; '
+                     'yabs-sid=2627592021651080827'}
