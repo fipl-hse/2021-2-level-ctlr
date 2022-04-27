6c0f115160c907400adde22f447069804324fa02
IrinaKurnakova <ikurnakova2002@mail.ru>
2022-04-27 17:51:57 +0300

    changes for 10

--- pos_frequency_pipeline.py
+++ pos_frequency_pipeline.py
@@ -1,8 +1,13 @@
 """
 Implementation of POSFrequencyPipeline for score ten only.
 """
+import json
+import re
 
-from pipeline import CorpusManager
+from constants import ASSETS_PATH
+from core_utils.article import ArtifactType
+from core_utils.visualizer import visualize
+from pipeline import CorpusManager, validate_dataset
 
 
 class EmptyFileError(Exception):
@@ -13,15 +18,41 @@ class EmptyFileError(Exception):
 
 class POSFrequencyPipeline:
     def __init__(self, corpus_manager: CorpusManager):
-        pass
+        self.corpus_manager = corpus_manager
 
     def run(self):
-        pass
+        articles = self.corpus_manager.get_articles().values()
+
+        for article in articles:
+            path_to_article = article.get_file_path(ArtifactType.single_tagged)
+            with open(path_to_article, encoding='utf-8') as file:
+                text = file.read()
+
+            pattern = re.compile(r'<([A-Z]+)')
+            freq_dict = {}
+            parts_of_speech = pattern.findall(text)
+
+            for element in parts_of_speech:
+                if element not in freq_dict:
+                    freq_dict[element] = parts_of_speech.count(element)
+
+            with open(article.get_meta_file_path(), encoding='utf-8') as meta_file:
+                meta_data = json.load(meta_file)
+
+            with open(article.get_meta_file_path(), 'w', encoding='utf-8') as meta_file:
+                json.dump(meta_data, meta_file, ensure_ascii=False, indent=4, separators=(',', ':'))
+
+            meta_data['pos_frequencies'] = freq_dict
+
+            visualize(statistics=freq_dict, path_to_save=ASSETS_PATH / f'{article.article_id}_image.png')
 
 
 def main():
     # YOUR CODE HERE
-    pass
+    validate_dataset(ASSETS_PATH)
+    corpus_manager = CorpusManager(ASSETS_PATH)
+    pipeline = POSFrequencyPipeline(corpus_manager=corpus_manager)
+    pipeline.run()
 
 
 if __name__ == "__main__":
