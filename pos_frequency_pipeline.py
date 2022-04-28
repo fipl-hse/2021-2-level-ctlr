c929ab1a8b5d1552fce4a52ca6c6d928f3dde9ea
ArtyomBibikov <artembibikov75@gmail.com>
2022-04-28 23:22:25 +0300

    Add POS Frequency

--- pos_frequency_pipeline.py
+++ pos_frequency_pipeline.py
@@ -1,8 +1,13 @@
 """
 Implementation of POSFrequencyPipeline for score ten only.
 """
+import json
+import re
 
-from pipeline import CorpusManager
+from constants import ASSETS_PATH
+from core_utils.article import ArtifactType
+from core_utils.visualizer import visualize
+from pipeline import CorpusManager, validate_dataset
 
 
 class EmptyFileError(Exception):
@@ -13,15 +18,36 @@ class EmptyFileError(Exception):
 
 class POSFrequencyPipeline:
     def __init__(self, corpus_manager: CorpusManager):
-        pass
+        self.corpus_manager = corpus_manager
 
     def run(self):
-        pass
+        for article in self.corpus_manager.get_articles().values():
+            frequency_dict = {}
+            with open(article.get_file_path(ArtifactType.single_tagged), 'r', encoding='utf-8') as file:
+                text = file.read()
+            if not text:
+                raise EmptyFileError
+            pattern = re.compile(r'<([A-Z]+)')
+            all_tags = pattern.findall(text)
+            for tag in all_tags:
+                if tag not in frequency_dict:
+                    frequency_dict[tag] = 1
+                else:
+                    frequency_dict[tag] += 1
+            with open(ASSETS_PATH / article.get_meta_file_path(), 'r', encoding='utf-8') as file:
+                data = json.load(file)
+            data.update({'pos_frequencies': frequency_dict})
+            with open(ASSETS_PATH / article.get_meta_file_path(), 'w', encoding='utf-8') as meta_file:
+                json.dump(data, meta_file, sort_keys=False,
+                          indent=4, ensure_ascii=False, separators=(',', ': '))
+            visualize(statistics=frequency_dict, path_to_save=ASSETS_PATH / f'{article.article_id}_image.png')
 
 
 def main():
-    # YOUR CODE HERE
-    pass
+    validate_dataset(ASSETS_PATH)
+    corpus_manager = CorpusManager(ASSETS_PATH)
+    pipeline = POSFrequencyPipeline(corpus_manager=corpus_manager)
+    pipeline.run()
 
 
 if __name__ == "__main__":
