d6c3e9cd452ece2c06a9061dc3359df5795dc7b8
Anastasia <awake777.20@gmail.com>
2022-04-28 19:21:40 +0300

    is it a problem?

--- scrapper.py
+++ scrapper.py
@@ -197,7 +197,7 @@ def validate_config(crawler_path):
             raise IncorrectURLError
     if not isinstance(number_of_articles, int) or number_of_articles <= 0:
         raise IncorrectNumberOfArticlesError
-    if number_of_articles > 1:
+    if number_of_articles > 100:
         raise NumberOfArticlesOutOfRangeError
     return seed_urls, number_of_articles
 
