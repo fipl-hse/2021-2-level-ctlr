55895e1d26659f29bdd7943af4e31b0e1e741f10
Styagova Maria <90442713+styagovamaria@users.noreply.github.com>
2022-04-28 12:40:35 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -7,3 +7,7 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).parent
 ASSETS_PATH = PROJECT_ROOT / 'tmp' / 'articles'
 CRAWLER_CONFIG_PATH = PROJECT_ROOT / 'scrapper_config.json'
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+    'Chrome/89.0.4389.86 YaBrowser/21.3.1.185 Yowser/2.5 Safari/537.36'
+    } 
