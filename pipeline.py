2188ef0390b94f1f9edb3cfc27ed75e1c6708c99
EIzharikov <egor.zharikov@inbox.ru>
2022-04-28 20:17:00 +0300

    fixing problems with validating dataset

--- pipeline.py
+++ pipeline.py
@@ -138,21 +138,21 @@ def validate_dataset(path_to_validate):
     path_to_validate = Path(path_to_validate)
 
     if not path_to_validate.exists():
-        raise FileNotFoundError
+        raise FileNotFoundError('Path not exits')
 
     if not path_to_validate.is_dir():
-        raise NotADirectoryError
+        raise NotADirectoryError('Path is not a directory')
 
     counter_txt = 0
     for file in path_to_validate.glob('*'):
         if file.stat().st_size == 0:
-            raise InconsistentDatasetError
+            raise InconsistentDatasetError(f'{file.name} is empty')
         if file.name.endswith('raw.txt'):
             counter_txt += 1
             path_for_meta = path_to_validate / f'{counter_txt}_meta.txt'
             path_for_txt = path_to_validate / f'{counter_txt}_raw.txt'
             if not path_for_txt or not path_for_meta:
-                raise InconsistentDatasetError()
+                raise InconsistentDatasetError
 
     pattern_for_filename = re.compile(r'(\d+)_(cleaned.txt|meta.json|multiple_tagged.txt|raw.txt|raw.pdf'
                                       r'|single_tagged.txt)')
@@ -160,20 +160,30 @@ def validate_dataset(path_to_validate):
     for file in path_to_validate.glob('*'):
         match_for_name = pattern_for_filename.match(file.name)
         if not match_for_name:
-            raise InconsistentDatasetError("Incorrect file name")
+            raise InconsistentDatasetError('Incorrect file name')
         all_ids.append(int(match_for_name.group(1)))
     if not all_ids:
-        raise EmptyDirectoryError
+        raise EmptyDirectoryError('Directory is empty!')
 
     sorted_all_ids = sorted(all_ids)
     previous_id_of_file = 0
     for id_of_file in sorted_all_ids:
         if id_of_file - previous_id_of_file > 1:
-            raise InconsistentDatasetError
+            raise InconsistentDatasetError('Dataset is inconsistent')
         previous_id_of_file = id_of_file
 
     if sorted_all_ids[0] != 1:
         raise InconsistentDatasetError()
+
+    counter_txt = 0
+    counter_meta = 0
+    for file in path_to_validate.glob('*'):
+        if file.name.endswith('raw.txt'):
+            counter_txt += 1
+        elif file.name.endswith('meta.json'):
+            counter_meta += 1
+    if counter_txt != counter_meta:
+        raise InconsistentDatasetError('Dataset is imbalanced')
 
 
 def main():
