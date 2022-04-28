c31e81f44541f6eee91233f4773e805945178c44
Styagova Maria <90442713+styagovamaria@users.noreply.github.com>
2022-04-28 15:08:42 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,12 +2,12 @@
 Useful constant variables
 """
 
-from pathlib import Path
+import os
 
-PROJECT_ROOT = Path(__file__).parent
-ASSETS_PATH = PROJECT_ROOT / 'tmp' / 'articles'
-CRAWLER_CONFIG_PATH = PROJECT_ROOT / 'scrapper_config.json'
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+ASSETS_PATH = os.path.join(PROJECT_ROOT, 'tmp', 'articles')
+CRAWLER_CONFIG_PATH = os.path.join(PROJECT_ROOT, 'crawler_config.json')
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
     'Chrome/89.0.4389.86 YaBrowser/21.3.1.185 Yowser/2.5 Safari/537.36'
-    } 
+    }
