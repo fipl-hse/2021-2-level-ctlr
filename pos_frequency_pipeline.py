6d2fc943737e59c50e97757069c6f190b5084cb7
DangerousCrocodile <90447330+DangerousCrocodile@users.noreply.github.com>
2022-04-29 14:19:21 +0300

    ahahahah

--- pos_frequency_pipeline.py
+++ pos_frequency_pipeline.py
@@ -22,14 +22,14 @@ class POSFrequencyPipeline:
         self.corpus_manager = corpus_manager
         self.pos_freq = {}
 
-    def find_tags(self, article):
+    def _find_tags(self, article):
         path_to_article = article.get_file_path(ArtifactType.single_tagged)
 
         with open(Path(path_to_article), "r", encoding='utf-8') as file:
             tags = file.read()
             if not tags:
                 raise EmptyFileError
-        return tags
+            return tags
 
     def _pos_counter(self, tags):
         pattern = re.compile(r'<([A-Z]+)')
@@ -46,9 +46,10 @@ class POSFrequencyPipeline:
 
     def run(self):
         for article in self.corpus_manager.get_articles().values():
-            self._pos_counter(self.find_tags(article))
+            self._pos_counter(self._find_tags(article))
             self._update_meta(article)
             visualize(self.pos_freq, path_to_save=ASSETS_PATH / f'{article.article_id}_image.png')
+            self.pos_freq.clear()
 
 
 def main():
