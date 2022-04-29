7210704c1dbc068ac1ffd014410c45f2481c6fd4
DangerousCrocodile <90447330+DangerousCrocodile@users.noreply.github.com>
2022-04-29 14:49:18 +0300

    you can make it to the sunrise

--- pos_frequency_pipeline.py
+++ pos_frequency_pipeline.py
@@ -20,36 +20,37 @@ class EmptyFileError(Exception):
 class POSFrequencyPipeline:
     def __init__(self, corpus_manager: CorpusManager):
         self.corpus_manager = corpus_manager
-        self.pos_freq = {}
 
-    def _find_tags(self, article):
+    def validate_file_with_tags(self, article):
+        path_to_article = article.get_file_path(ArtifactType.single_tagged)
+        if Path(path_to_article).stat().st_size == 0:
+            raise EmptyFileError
+
+    def _pos_counter(self, article):
+        pos_freq = {}
+
         path_to_article = article.get_file_path(ArtifactType.single_tagged)
 
         with open(Path(path_to_article), "r", encoding='utf-8') as file:
             tags = file.read()
-            if not tags:
-                raise EmptyFileError
-            return tags
-
-    def _pos_counter(self, tags):
         pattern = re.compile(r'<([A-Z]+)')
         for pos in pattern.findall(tags):
-            self.pos_freq[pos] = self.pos_freq.get(pos, 0) + 1
+            pos_freq[pos] = pos_freq.get(pos, 0) + 1
+        return pos_freq
 
-    def _update_meta(self, article):
+    def _update_meta(self, article, pos_freq):
         with open(Path(article.get_meta_file_path()), 'r', encoding='utf-8') as meta_file:
             meta = json.load(meta_file)
-        meta.update({"pos_frequencies": self.pos_freq})
+        meta.update({"pos_frequencies": pos_freq})
         with open(Path(article.get_meta_file_path()), 'w', encoding='utf-8') as new_meta_file:
             json.dump(meta, new_meta_file, sort_keys=False,
                       indent=4, ensure_ascii=False, separators=(',', ': '))
 
     def run(self):
         for article in self.corpus_manager.get_articles().values():
-            self._pos_counter(self._find_tags(article))
-            self._update_meta(article)
-            visualize(self.pos_freq, path_to_save=ASSETS_PATH / f'{article.article_id}_image.png')
-            self.pos_freq.clear()
+            self.validate_file_with_tags(article)
+            self._update_meta(article, self._pos_counter(article))
+            visualize(self._pos_counter(article), path_to_save=ASSETS_PATH / f'{article.article_id}_image.png')
 
 
 def main():
