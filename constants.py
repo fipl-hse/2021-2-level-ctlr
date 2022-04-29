3d9d7faf0743407d5db731fe2cfd5853adc1dbaf
Kate <kate2002elisey@gmail.com>
2022-04-29 16:29:59 +0300

    under pressurex23

--- constants.py
+++ constants.py
@@ -7,6 +7,6 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).parent
 ASSETS_PATH = PROJECT_ROOT / 'tmp' / 'articles'
 CRAWLER_CONFIG_PATH = PROJECT_ROOT / 'scrapper_config.json'
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/'
-                  '537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                         '(KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36 OPR/85.0.4341.72'}
 URL_PATTERN = "https://www.politlinguistika.ru"
