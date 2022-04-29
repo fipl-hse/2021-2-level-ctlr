0674ef53460f5b63c205f9233031c0da0b97b455
eapareshina <zhenya.pareshina@gmail.com>
2022-04-29 10:27:57 +0300

    changes

--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,14 @@
 """
 Pipeline for text processing implementation
 """
-
+import re
 from pathlib import Path
 
-# import pymorphy2
-# from pymystem3 import Mystem
+from pymystem3 import Mystem
+from pymorphy2 import MorphAnalyzer
 
 from constants import ASSETS_PATH
-from core_utils.article import Article
+from core_utils.article import Article, ArtifactType
 
 
 class EmptyDirectoryError(Exception):
@@ -32,25 +32,28 @@ class MorphologicalToken:
     """
 
     def __init__(self, original_word):
-        pass
+        self.original_word = original_word
+        self.normalized_form = ''
+        self.tags_mystem = ''
+        self.tags_pymorphy = ''
 
     def get_cleaned(self):
         """
         Returns lowercased original form of a token
         """
-        pass
+        return self.original_word.lower()
 
     def get_single_tagged(self):
         """
         Returns normalized lemma with MyStem tags
         """
-        pass
+        return f"{self.normalized_form}<{self.tags_mystem}>"
 
     def get_multiple_tagged(self):
         """
         Returns normalized lemma with PyMorphy tags
         """
-        pass
+        return f"{self.normalized_form}<{self.tags_mystem}>({self.tags_pymorphy})"
 
 
 class CorpusManager:
@@ -67,11 +70,11 @@ class CorpusManager:
         """
         Register each dataset entry
         """
-        path_to_raws = Path(self.path_to_raw_txt_data)
-        dataset = list(path_to_raws.rglob('*_raw.txt'))
+        path_to_raws = Path(self.path_to_raw_txt_data).glob('*_raw.txt')
+        pattern = re.compile(r'\d+')
 
-        for file in dataset:
-            article_id = int(file.parts[-1].split('_')[0])
+        for file in path_to_raws:
+            article_id = int(pattern.search(file.name).group())
             self._storage[article_id] = Article(url=None, article_id=article_id)
 
     def get_articles(self):
@@ -87,20 +90,47 @@ class TextProcessingPipeline:
     """
 
     def __init__(self, corpus_manager: CorpusManager):
-        pass
+        self.corpus_manager = corpus_manager
 
     def run(self):
         """
         Runs pipeline process scenario
         """
-        pass
+        articles = self.corpus_manager.get_articles().values()
+
+        for article in articles:
+            tokens = self._process(article.get_raw_text())
+            tokens_for_article = []
+            single_tagged_tokens = []
+            multiple_tagged_tokens = []
+            for token in tokens:
+                tokens_for_article.append(token.get_cleaned())
+                single_tagged_tokens.append(token.get_single_tagged())
+                multiple_tagged_tokens.append(token.get_multiple_tagged())
+            article.save_as(' '.join(tokens_for_article), ArtifactType.cleaned)
+            article.save_as(' '.join(single_tagged_tokens), ArtifactType.single_tagged)
+            article.save_as(' '.join(multiple_tagged_tokens), ArtifactType.multiple_tagged)
 
     def _process(self, raw_text: str):
         """
         Processes each token and creates MorphToken class instance
         """
-        # cleaned_text = ' '.join(re.findall(r'[а-яёА-ЯЁ]+', raw_text))
-        # analyzed_cleaned_text = Mystem().analyze(cleaned_text)
+        cleaned_text = ' '.join(re.findall(r'[а-яёА-ЯЁ]+', raw_text))
+        analyzed_cleaned_text = Mystem().analyze(cleaned_text)
+        tokens = []
+        morph = MorphAnalyzer()
+        for token in analyzed_cleaned_text:
+            if ('analysis' not in token) or (not token['analysis']) \
+                    or ('lex' not in token['analysis'][0] or 'gr' not in token['analysis'][0]):
+                continue
+            if ('text' not in token) or (not token['text']):
+                continue
+            morphological_token = MorphologicalToken(token['text'])
+            morphological_token.normalized_form = token['analysis'][0]['lex']
+            morphological_token.tags_mystem = token['analysis'][0]['gr']
+            morphological_token.tags_pymorphy = morph.parse(token['text'])[0].tag
+            tokens.append(morphological_token)
+        return tokens
 
 
 def validate_dataset(path_to_validate):
@@ -115,33 +145,37 @@ def validate_dataset(path_to_validate):
     if not path_to_dataset.is_dir():
         raise NotADirectoryError
 
-    list_of_txts = list(path_to_dataset.glob('*_raw.txt'))
-    list_of_jsons = list(path_to_dataset.glob('*_meta.json'))
+    if not list(path_to_dataset.iterdir()):
+        raise EmptyDirectoryError
 
-    if not len(list_of_txts) == len(list_of_jsons):
+    txts = path_to_dataset.glob('*_raw.txt')
+    jsons = path_to_dataset.glob('*_meta.json')
+
+    if not len(list(txts)) == len(list(jsons)):
         raise InconsistentDatasetError
+
+    digit_pattern = re.compile(r'\d+')
 
     txt_indices = []
     json_indices = []
-    for txt, json in zip(list_of_txts, list_of_jsons):
-        txt_indices.append(int(txt.parts[-1].split('_')[0]))
-        json_indices.append(int(json.parts[-1].split('_')[0]))
 
-        with open(txt, 'r', encoding='utf-8') as txt_file:
-            text = txt_file.read()
-        if not text:
+    for txt, json in zip(txts, jsons):
+        txt_indices.append(int(digit_pattern.search(txt.name).group()))
+        json_indices.append(int(digit_pattern.search(json.name).group()))
+
+    for file in path_to_dataset.iterdir():
+        if file.stat().st_size == 0:
             raise InconsistentDatasetError
 
     if sorted(txt_indices) != sorted(json_indices):
         raise InconsistentDatasetError
 
-    if not list(path_to_dataset.iterdir()):
-        raise EmptyDirectoryError
-
 
 def main():
     validate_dataset(ASSETS_PATH)
-    # corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
+    corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
+    pipeline = TextProcessingPipeline(corpus_manager=corpus_manager)
+    pipeline.run()
 
 
 if __name__ == "__main__":
