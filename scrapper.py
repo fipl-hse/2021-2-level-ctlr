b398f84f3fafd2f35d7f76f187d273884fd4dc91
DaryaKhodyreva <dashanasha2002@gmail.com>
2022-04-29 13:39:51 +0300

    try9

--- scrapper.py
+++ scrapper.py
@@ -68,7 +68,7 @@ class Crawler:
                 if len(self.urls) < self.max_articles:
                     if url not in self.urls:
                         self.urls.append(url)
-            sleep(3)
+            sleep(2)
 
     def get_search_urls(self):
         """
