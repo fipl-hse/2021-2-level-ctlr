a99e1a17abf206666cb85d37925b083e8325448b
DaryaKhodyreva <dashanasha2002@gmail.com>
2022-04-29 15:41:44 +0300

    ()

--- scrapper.py
+++ scrapper.py
@@ -58,7 +58,7 @@ class Crawler:
         """
         for seed_url in self.seed_urls:
             # sleep(random.randint(1, 3))
-            response = requests.get(url=seed_url)
+            response = requests.get(url=seed_url, timeout=25)
 
             if not response.ok:
                 continue
